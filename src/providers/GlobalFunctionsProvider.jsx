36e7c70ea3f31922ed00f07522cd9ff0c9ef956a
khamidullaevzafar <khamidullaevzafar@gmail.com>
2022-06-24 11:14:24 +0500

    Fixed bug

--- src/providers/GlobalFunctionsProvider.jsx
+++ src/providers/GlobalFunctionsProvider.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchApplicationListActions } from "../store/application/application.thunk";
 
 
 const GlobalFunctionsProvider = () => {
   const dispatch = useDispatch()
+  const isAuth = useSelector(state => state.auth.isAuth)
 
   useEffect(() => {
+    if(!isAuth) return
+
     dispatch(fetchApplicationListActions())
-  }, [dispatch])
+  }, [dispatch, isAuth])
 
 
   return null;

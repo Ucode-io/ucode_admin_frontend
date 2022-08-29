60a878d765e3350fb44e382ba3735c2f37a00d93
Munis <Muniss138@gmail.com>
2022-08-29 17:32:34 +0500

    adjust permissions to connection name case

--- src/components/PermissionWrapper/PermissionWrapperV2.jsx
+++ src/components/PermissionWrapper/PermissionWrapperV2.jsx
@@ -4,17 +4,16 @@ const PermissionWrapperV2 = ({ children, tabelSlug = "", type }) => {
   const permissions = useSelector((state) => state.auth.permissions);
   if (!tabelSlug) return children;
 
-
   if (!tabelSlug) return null;
 
   if (typeof type === "object") {
-    if (!permissions[tabelSlug][type[0]] && !permissions[tabelSlug][type[1]])
-      return null;
+    if (permissions[tabelSlug][type[0]] && permissions[tabelSlug][type[1]])
+      return children;
 
-    return children;
+    return null;
   } else {
-    if (!permissions[tabelSlug]?.[type]) return null;
-    return children;
+    if (permissions[tabelSlug]?.[type]) return children;
+    return null;
   }
 };
 

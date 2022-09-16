383eac3b6b17db420b8872c7a6dcefbbc131f475
khamidullaevzafar <khamidullaevzafar@gmail.com>
2022-09-16 15:06:02 +0500

    Fixed bug

---
+++ src/utils/dateValidFormat.js
@@ -0,0 +1,11 @@
+import { format, isValid } from "date-fns"
+
+
+
+export const dateValidFormat = (date, formatString) => {
+  if(date) return ''
+
+  if(!isValid(new Date(date))) return ''
+
+  return format(date, formatString)
+}

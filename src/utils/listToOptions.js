2d69baeb625af84d4b267abfc08a4105f605ad58
Baha <bahrid@gmail.com>
2022-10-10 14:27:59 +0500

    autofill is finished

--- src/utils/listToOptions.js
+++ src/utils/listToOptions.js
@@ -1,7 +1,7 @@
 
 
-const listToOptions = (list, labelFieldName = 'title', valueFieldName = 'id', type = 'type', relation_id ='relation_id') => {
-  return list?.map(el => ({ value: valueFieldName !== 'all' ? el[valueFieldName] : el, label: el[labelFieldName], type: el[type], relation_id: el[relation_id] })) ?? []
+const listToOptions = (list, labelFieldName = 'title', valueFieldName = 'id') => {
+  return list?.map(el => ({ value: valueFieldName !== 'all' ? el[valueFieldName] : el, label: el[labelFieldName] })) ?? []
 }
 
 export default listToOptions

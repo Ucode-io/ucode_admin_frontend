f42db120341605736a4be3de156ad44b896b4898
Baha <bahrid@gmail.com>
2022-10-17 11:16:55 +0500

    ui bug is fixed

--- src/components/FiltersBlock/index.jsx
+++ src/components/FiltersBlock/index.jsx
@@ -2,11 +2,7 @@ import "./style.scss";
 
 const FiltersBlock = ({ children, extra, ...props }) => {
   return (
-    <div
-      className="FiltersBlock"
-      style={{ height: props.summary ? "70px" : "" }}
-      {...props}
-    >
+    <div className="FiltersBlock" {...props}>
       <div className="side">{children}</div>
       <div className="side"> {extra}</div>
     </div>

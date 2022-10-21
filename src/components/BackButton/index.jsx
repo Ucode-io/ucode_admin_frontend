faa240ae680fe20786713597e2a7fbe7bf3fc9dd
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2022-10-21 11:10:46 +0500

    settigns object back button go back on click bug fixed

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -1,11 +1,10 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack"
 import "./style.scss"
 
-const BackButton = ({ className = '', ...props }) => {
-
+const BackButton = ({ className = "", ...props }) => {
   return (
-    <button type="button" className={`BackButton ${className}`} {...props} >
-      <ArrowBackIcon className="icon"  />
+    <button type="button" className={`BackButton ${className}`} {...props}>
+      <ArrowBackIcon className="icon" />
     </button>
   )
 }
--- src/views/AuthMatrix/ClientType/index.jsx
+++ src/views/AuthMatrix/ClientType/index.jsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Header from "../../../components/Header";
-import HeaderSettings from "../../../components/HeaderSettings";
-import clientTypeService from "../../../services/auth/clientTypeService";
-import InfoFieldsSection from "./InfoFieldsSection";
-import RelationsSection from "./RelationsSection";
-import "./style.scss";
-import TablesSection from "./TablesSection";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import Header from "../../../components/Header"
+import HeaderSettings from "../../../components/HeaderSettings"
+import clientTypeService from "../../../services/auth/clientTypeService"
+import InfoFieldsSection from "./InfoFieldsSection"
+import RelationsSection from "./RelationsSection"
+import "./style.scss"
+import TablesSection from "./TablesSection"
 
 const ClientType = () => {
-  const { typeId, projectId } = useParams();
+  const { typeId, projectId } = useParams()
 
-  const [loader, setLoader] = useState(true);
-  const [rolesList, setRolesList] = useState([]);
-  const [fieldsList, setFieldsList] = useState([]);
-  const [relationsList, setRelationsList] = useState([]);
-  const [data, setData] = useState({});
-  const [tables, setTables] = useState([]);
+  const [loader, setLoader] = useState(true)
+  const [rolesList, setRolesList] = useState([])
+  const [fieldsList, setFieldsList] = useState([])
+  const [relationsList, setRelationsList] = useState([])
+  const [data, setData] = useState({})
+  const [tables, setTables] = useState([])
 
   const getData = () => {
-    setLoader(true);
+    setLoader(true)
 
     clientTypeService
       .getById(typeId)
       .then((res) => {
-        setData(res.client_type ?? {});
-        setTables(res.client_type?.tables ?? []);
-        setRolesList(res.roles ?? []);
-        setFieldsList(res.user_info_fields ?? []);
-        setRelationsList(res.relations ?? []);
+        setData(res.client_type ?? {})
+        setTables(res.client_type?.tables ?? [])
+        setRolesList(res.roles ?? [])
+        setFieldsList(res.user_info_fields ?? [])
+        setRelationsList(res.relations ?? [])
       })
-      .finally(() => setLoader(false));
-  };
+      .finally(() => setLoader(false))
+  }
 
-  console.log("DATA ===>", data);
+  console.log("DATA ===>", data)
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
     <div className="ClientType">
@@ -60,7 +60,7 @@ const ClientType = () => {
         /> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ClientType;
+export default ClientType

32247bca97bfbda9e7202d45082015d7b2193c3f
Zafar Khamidullaev <khamidullaevzafar@gmail.com>
2023-02-15 20:58:43 +0500

    Fixed bug at many2many cell

--- src/components/ElementGenerators/Many2ManyValue.jsx
+++ src/components/ElementGenerators/Many2ManyValue.jsx
@@ -1,37 +1,30 @@
-import React, { useMemo } from 'react';
-import { useQuery } from 'react-query';
-import constructorObjectService from '../../services/constructorObjectService';
+import get from "lodash.get";
+import { useMemo } from "react";
+import { useQuery } from "react-query";
+import constructorObjectService from "../../services/constructorObjectService";
 
-function Many2ManyValue({field, value}) {
-    
-    const { data: options } = useQuery(
-        ["GET_OBJECT_LIST", field?.table_slug],
-        () => {
-          return constructorObjectService.getList(field?.table_slug, {
-            data: {
-             guid: value
-            },
-          });
+function Many2ManyValue({ field, value }) {
+  const { data: options = [] } = useQuery(
+    ["GET_OBJECT_LIST", field?.table_slug],
+    () => {
+      return constructorObjectService.getList(field?.table_slug, {
+        data: {
+          guid: value,
         },
-        {
-          select: (res) => {
-            return res?.data?.response ?? [];
-          },
-        }
-      );
-      const computedValue = useMemo(() => {
-        let val = '';
-        const slugs = field?.view_fields?.map((item) => item?.slug)
-         options?.map((item) => {
-           return val += slugs?.map((el) => " " +  item?.[el] + ', ')
-        })
-        return val
-      }, [field, options])
-    return (
-        <div>
-            {computedValue}
-        </div>
-    );
+      });
+    },
+    {
+      select: (res) => {
+        return res?.data?.response ?? [];
+      },
+    }
+  );
+  const computedValue = useMemo(() => {
+    const slugs = field?.view_fields?.map((item) => item?.slug) ?? [];
+    return options.map((option) => slugs.map((slug) => get(option, slug, '')).join(' ')).join(', ');
+  }, [field, options]);
+
+  return computedValue
 }
 
 export default Many2ManyValue;

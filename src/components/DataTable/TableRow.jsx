cddccf9312b6b6a0d81170b238e525a4e2c1ee71
nurmatjon_abdullayev <anstronnna@gmail.com>
2023-03-27 12:06:44 +0500

    add colorCell function

--- src/components/DataTable/TableRow.jsx
+++ src/components/DataTable/TableRow.jsx
@@ -33,6 +33,15 @@ const TableRow = ({
   onChecked,
   relationFields,
 }) => {
+  const colorCell = (num) => {
+    if (num === 0) {
+      return "rgba(3, 172, 19,0.5)";
+    }
+    if (num < 0 || num > 0) {
+      return "rgba(254 , 0, 0,0.5)";
+    }
+    return "transparent";
+  };
   if (formVisible)
     return (
       <TableRowForm
@@ -174,6 +183,7 @@ const TableRow = ({
                 )?.isStiky
                   ? "1"
                   : "",
+                backgroundColor: colorCell(row?.difference),
               }}
             >
               <CellElementGenerator field={column} row={row} />

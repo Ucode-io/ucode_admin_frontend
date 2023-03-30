829f839dd0e0bf5e7237c3c7e191dde7b313f8aa
Shohrux99 <b.shohrux@mail.ru>
2023-03-30 16:53:09 +0500

    revert to v1 in custom event

--- src/services/constructorCustomEventService.js
+++ src/services/constructorCustomEventService.js
@@ -1,13 +1,11 @@
-
 import request from "../utils/request";
-import requestV2 from "../utils/requestV2.js";
 
 
 const constructorCustomEventService = {
-  getList: (params) => requestV2.get('/custom-event', { params }),
-  update: (data) => requestV2.put('/custom-event', data),
-  create: (data) => requestV2.post('/custom-event', data),
-  delete: (id, data) => requestV2.delete(`/custom-event/${id}`, data),
+  getList: (params) => request.get('/custom-event', { params }),
+  update: (data) => request.put('/custom-event', data),
+  create: (data) => request.post('/custom-event', data),
+  delete: (id, data) => request.delete(`/custom-event/${id}`, data),
 }
 
 export default constructorCustomEventService;

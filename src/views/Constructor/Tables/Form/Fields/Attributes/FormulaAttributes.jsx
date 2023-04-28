252828eaed98064e6d488b7cff8f4f68f465afc3
Baha <bahrid@gmail.com>
2023-04-28 15:24:26 +0500

    formula average is added

--- src/views/Constructor/Tables/Form/Fields/Attributes/FormulaAttributes.jsx
+++ src/views/Constructor/Tables/Form/Fields/Attributes/FormulaAttributes.jsx
@@ -95,7 +95,7 @@ const FormulaAttributes = ({ control, mainForm }) => {
           />
         </FRow>
 
-        {(type === "SUMM" || type === "MAX") && (
+        {(type === "SUMM" || type === "MAX" || type === 'AVG') && (
           <>
             <FRow label="Table from">
               <HFSelect

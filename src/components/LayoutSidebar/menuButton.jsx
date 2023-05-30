cfaefaad79194e5d0105130923b9e1b0593dac40
Moxrbe <one393752@gmail.com>
2023-05-30 18:02:01 +0500

    replace profile, chat

--- src/components/LayoutSidebar/menuButton.jsx
+++ src/components/LayoutSidebar/menuButton.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
 import "./style.scss";
 
-const MenuButton = ({ onClick, title, icon }) => {
+const MenuButton = ({ onClick, title, icon, children }) => {
   return (
     <Button className="menu-button active-with-child" onClick={onClick}>
+      {children}
       <div className="label">
         {icon}
         {title}

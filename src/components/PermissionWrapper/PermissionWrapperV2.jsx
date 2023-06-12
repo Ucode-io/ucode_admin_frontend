3cddf0f0114e82815c36ff2daf4b18aae8631668
Shohrux99 <b.shohrux@mail.ru>
2023-06-12 12:33:26 +0500

    fixed loop in view settings

--- src/components/PermissionWrapper/PermissionWrapperV2.jsx
+++ src/components/PermissionWrapper/PermissionWrapperV2.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 const PermissionWrapperV2 = ({ children, tableSlug, type }) => {
   const permissions = useSelector((state) => state.auth.permissions);
   const role = useSelector((state) => state.auth.roleInfo);
-  // if (!tableSlug || role?.name === "DEFAULT ADMIN") return children;
+  if (!tableSlug || role?.name === "DEFAULT ADMIN") return children;
 
   if (typeof type === "object") {
     if (permissions?.[tableSlug]?.[type[0]] && permissions?.[tableSlug]?.[type[1]]) return children;

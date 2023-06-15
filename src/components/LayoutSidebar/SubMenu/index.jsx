14e968c022b88abf45e4a494147f4b5c8b942943
Moxrbe <one393752@gmail.com>
2023-06-15 12:03:11 +0500

    fix submenu

--- src/components/LayoutSidebar/SubMenu/index.jsx
+++ src/components/LayoutSidebar/SubMenu/index.jsx
@@ -23,9 +23,6 @@ const SubMenu = ({
   setElement,
   selectedApp,
 }) => {
-  console.log("element===>", element);
-  console.log("selectedTable===>", selectedTable);
-  console.log("selectedApp===>", selectedApp);
   return (
     <div className={`SubMenu ${!subMenuIsOpen ? "right-side-closed" : ""}`}>
       <div className="header" onClick={() => {}}>
@@ -49,7 +46,6 @@ const SubMenu = ({
                 handleOpenNotify(e, "CREATE_TO_FOLDER");
                 setSelectedTable(element);
                 setElement(element);
-                console.log("eeeee", e.target);
               }}
               style={{
                 color: environment?.data?.color,

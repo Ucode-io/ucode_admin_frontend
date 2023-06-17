d5380855d44c368f866975d192b2bd2f0adae8b9
Baha <bahrid@gmail.com>
2023-06-17 11:27:00 +0500

    api key is added

--- src/components/FormElements/HFMapField.jsx
+++ src/components/FormElements/HFMapField.jsx
@@ -1,8 +1,13 @@
 import { Box } from "@mui/material";
-import { Map, Placemark, YMaps } from "@pbe/react-yandex-maps";
+import {
+  Map,
+  Placemark,
+  YMaps,
+} from "@pbe/react-yandex-maps";
 import { toNumber } from "lodash-es";
 import { useEffect, useRef, useState } from "react";
-import { Controller, useWatch } from "react-hook-form";
+import { Controller } from "react-hook-form";
+import styles from './style.module.scss'
 
 const HFMapField = ({
   control,
@@ -18,8 +23,25 @@ const HFMapField = ({
   const mapRef = useRef();
   const [selectedCoordinates, setSelectedCoordinates] = useState({
     lat: 55.76,
-    long: 37.64
+    long: 37.64,
   });
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setSelectedCoordinates({ lat: latitude, long: longitude });
+        },
+        (error) => {
+          console.error("Error getting current location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported in this browser.");
+    }
+  }, []);
+
 
   return (
     <Controller
@@ -28,7 +50,7 @@ const HFMapField = ({
       defaultValue=""
       rules={{
         required: required ? "This is a required field" : false,
-        ...rules
+        ...rules,
       }}
       render={({ field: { onChange, value }, fieldState: { error } }) => {
         const lat = selectedCoordinates.lat || value?.split(",")?.[0];
@@ -40,25 +62,28 @@ const HFMapField = ({
           if (attributesLat && attributesLong) {
             setSelectedCoordinates({
               lat: attributesLat,
-              long: attributesLong
+              long: attributesLong,
             });
           }
         }
 
         return (
-          <Box sx={{ width: "275px", overflow: "hidden" }}>
-            <YMaps query={{ load: "package.full" }}>
+          <Box sx={{ width: "275px", overflow: "hidden", position: 'relative' }}>
+            <YMaps query={{ load: "package.full", apikey: '1e36415e-d69c-4ac7-a475-421ac7416d16' }}>
               <Map
                 style={{ width: "275px", height: "200px" }}
                 defaultState={{
                   center: [lat, long],
-                  zoom: 7
+                  zoom: 7,
                 }}
                 instanceRef={mapRef}
                 onClick={(e) => {
                   const [clickedLat, clickedLng] = e.get("coords");
                   setSelectedCoordinates({ lat: clickedLat, long: clickedLng });
                   onChange(`${clickedLat},${clickedLng}`);
+                }}
+                options={{
+                  suppressMapOpenBlock: true,
                 }}
               >
                 <Placemark geometry={[lat, long]} />

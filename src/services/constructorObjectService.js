24b14ee37f255dd37a6a231dbc7c310ff45267e1
Shohrux99 <b.shohrux@mail.ru>
2023-08-23 11:35:54 +0500

    some changes bug fixed

--- src/services/constructorObjectService.js
+++ src/services/constructorObjectService.js
@@ -2,31 +2,21 @@ import { useQuery } from "react-query";
 import request from "../utils/request";
 
 const constructorObjectService = {
-  getList: (tableSlug, data) =>
-    request.post(`/object/get-list/${tableSlug}`, data),
-  getAutofilterList: ({ tableSlug, ...params }, data) =>
-    request.post(`/object/get-list/${tableSlug}`, data, { params }),
+  getList: (tableSlug, data) => request.post(`/object/get-list/${tableSlug}`, data),
+  groupByList: (tableSlug, rowTableSlug, data, params) => request.post(`/object/get-list-group-by/${tableSlug}/${rowTableSlug}`, data, { params }),
+  getAutofilterList: ({ tableSlug, ...params }, data) => request.post(`/object/get-list/${tableSlug}`, data, { params }),
   update: (tableSlug, data) => request.put(`/object/${tableSlug}`, data),
-  updateMultiple: (tableSlug, data) =>
-    request.post(`/object-upsert/${tableSlug}`, data),
+  updateMultiple: (tableSlug, data) => request.post(`/object-upsert/${tableSlug}`, data),
   create: (tableSlug, data) => request.post(`/object/${tableSlug}`, data),
   getById: (tableSlug, id) => request.get(`/object/${tableSlug}/${id}`),
-  delete: (tableSlug, id) =>
-    request.delete(`/object/${tableSlug}/${id}`, { data: { data: {} } }),
+  delete: (tableSlug, id) => request.delete(`/object/${tableSlug}/${id}`, { data: { data: {} } }),
   updateManyToMany: (data) => request.put("/many-to-many", data),
-  updateMultipleObject: (tableSlug, data) =>
-    request.put(`/object/multiple-update/${tableSlug}`, data),
+  updateMultipleObject: (tableSlug, data) => request.put(`/object/multiple-update/${tableSlug}`, data),
   deleteManyToMany: (data) => request.delete("/many-to-many", { data }),
-  downloadExcel: (tableSlug, data) =>
-    request.post(`/object/excel/${tableSlug}`, data),
-  getFinancialAnalytics: (tableSlug, data) =>
-    request.post(`/object/get-financial-analytics/${tableSlug}`, data),
+  downloadExcel: (tableSlug, data) => request.post(`/object/excel/${tableSlug}`, data),
+  getFinancialAnalytics: (tableSlug, data) => request.post(`/object/get-financial-analytics/${tableSlug}`, data),
 };
-export const useObjectsListQuery = ({
-  params = {},
-  data = {},
-  queryParams,
-} = {}) => {
+export const useObjectsListQuery = ({ params = {}, data = {}, queryParams } = {}) => {
   return useQuery(
     ["OBJECTS", { ...params, ...data }],
     () => {

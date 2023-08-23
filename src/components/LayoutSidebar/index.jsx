23424a089870e41e98b493a93dd2a2544461c264
Bahriddin <bahridnurullav@gmail.com>
2023-08-23 12:24:38 +0500

    logo fontSize

--- src/components/LayoutSidebar/index.jsx
+++ src/components/LayoutSidebar/index.jsx
@@ -4,7 +4,7 @@ import { Box, Button, Divider } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Container } from "react-smooth-dnd";
 import { UdevsLogo } from "../../assets/icons/icon";
 import FolderCreateModal from "../../layouts/MainLayout/FolderCreateModal";
@@ -29,8 +29,8 @@ import SubMenu from "./SubMenu";
 import "./style.scss";
 import { useProjectGetByIdQuery } from "../../services/projectService";
 import MenuBox from "./Components/MenuBox";
-import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 const LayoutSidebar = ({ appId }) => {
   const sidebarIsOpen = useSelector(
@@ -212,39 +212,44 @@ const LayoutSidebar = ({ appId }) => {
         }}
       >
         <div className="header">
-          <div className="brand" onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
+          <div
+            className="brand"
+            onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+          >
             {projectInfo?.logo ? (
               <img src={projectInfo?.logo} alt="" width={40} height={40} />
             ) : (
               <UdevsLogo fill={"#007AFF"} />
             )}
 
-            {
-              !sidebarIsOpen && (
-                <Button style={{
-                  position: 'absolute',
-                  zIndex: '9',
-                  left: '58px',
-                  width: '20px',
-                  height: '20px',
-                  minWidth: '20px',
-                  borderRadius: '50%',
-                  border: '1px solid #e5e5e5',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  background: '#fff'
-                }}  >
-                  <KeyboardDoubleArrowRightIcon/>
-                </Button>
-              ) 
-            }
+            {!sidebarIsOpen && (
+              <Button
+                style={{
+                  position: "absolute",
+                  zIndex: "9",
+                  left: "58px",
+                  width: "20px",
+                  height: "20px",
+                  minWidth: "20px",
+                  borderRadius: "50%",
+                  border: "1px solid #e5e5e5",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  background: "#fff",
+                }}
+              >
+                <KeyboardDoubleArrowRightIcon />
+              </Button>
+            )}
 
             {sidebarIsOpen && (
               <h2
                 style={{
                   marginLeft: "8px",
                   color: menuStyle?.text || "#000",
+                  fontSize: "20px",
+                  fontWeight: "700",
                 }}
               >
                 {projectInfo?.title}
@@ -252,13 +257,11 @@ const LayoutSidebar = ({ appId }) => {
             )}
           </div>
 
-          {
-            sidebarIsOpen && (
-              <Button onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
-                <KeyboardDoubleArrowLeftIcon/>
-              </Button>
-            )
-          }
+          {sidebarIsOpen && (
+            <Button onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
+              <KeyboardDoubleArrowLeftIcon />
+            </Button>
+          )}
         </div>
 
         <Box

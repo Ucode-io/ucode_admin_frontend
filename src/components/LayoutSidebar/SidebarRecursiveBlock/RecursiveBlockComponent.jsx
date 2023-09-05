b843e40df37171d450817a27af1792da2965c4a6
Bahriddin <bahridnurullav@gmail.com>
2023-09-05 15:31:52 +0500

    conflict resolved

--- src/components/LayoutSidebar/SidebarRecursiveBlock/RecursiveBlockComponent.jsx
+++ src/components/LayoutSidebar/SidebarRecursiveBlock/RecursiveBlockComponent.jsx
@@ -2,19 +2,19 @@ import AddIcon from "@mui/icons-material/Add";
 import DeleteIconFromMui from "@mui/icons-material/Delete";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { Box, Button, Collapse, Tooltip } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { BsThreeDots } from "react-icons/bs";
-import { useMutation, useQueryClient } from "react-query";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import { useMenuListQuery } from "../../../services/menuService";
+import {Box, Button, Collapse, Tooltip} from "@mui/material";
+import {useEffect, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {BsThreeDots} from "react-icons/bs";
+import {useMutation, useQueryClient} from "react-query";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate, useParams} from "react-router-dom";
+import {useMenuListQuery} from "../../../services/menuService";
 import pivotService from "../../../services/pivotService";
-import { store } from "../../../store";
-import { showAlert } from "../../../store/alert/alert.thunk";
-import { menuActions } from "../../../store/menuItem/menuItem.slice";
-import { updateLevel } from "../../../utils/level";
+import {store} from "../../../store";
+import {showAlert} from "../../../store/alert/alert.thunk";
+import {menuActions} from "../../../store/menuItem/menuItem.slice";
+import {updateLevel} from "../../../utils/level";
 import IconGenerator from "../../IconPicker/IconGenerator";
 import ApiSidebar from "../Components/Api/ApiSidebar";
 import DataBase from "../Components/DataBase";
@@ -33,29 +33,8 @@ import Users from "../Components/Users";
 import DeleteIcon from "../DeleteIcon";
 import MenuIcon from "../MenuIcon";
 import "../style.scss";
-import { analyticItems, folderIds } from "./mock/folders";
-<<<<<<< HEAD
-=======
-import Permissions from "../Components/Permission";
-import DataBase from "../Components/DataBase";
-import EltResources from "../Components/Elt";
-import MicroServiceSidebar from "../Components/MicroService/MicroServiceSidebar";
-import ScenarioSidebar from "../Components/Scenario/ScenarioSidebar";
-import DocumentsSidebar from "../Components/Documents/DocumentsSidebar";
-import EmailSidebar from "../Components/Email/EmailSidebar";
-import ProjectSettingSidebar from "../Components/Project/ProjectSettingSidebar";
-import FunctionSidebar from "../Components/Functions/FunctionSIdebar";
-import NotificationSidebar from "../Components/Notification/NotificationSidebar";
-import Resources from "../Components/Resources";
-import { use } from "i18next";
-import { showAlert } from "../../../store/alert/alert.thunk";
-import pivotService from "../../../services/pivotService";
-import DeleteIcon from "../DeleteIcon";
-import ApiSidebar from "../Components/Api/ApiSidebar";
-import QuerySidebar from "../Components/Query/QuerySidebar";
-import { updateLevel } from "../../../utils/level";
+import {analyticItems, folderIds} from "./mock/folders";
 import MicrofrontendSettingSidebar from "../Components/Microfrontend/MicrofrontendSidebar";
->>>>>>> master_relation_bug
 export const adminId = `${import.meta.env.VITE_ADMIN_FOLDER_ID}`;
 export const analyticsId = `${import.meta.env.VITE_ANALYTICS_FOLDER_ID}`;
 
@@ -74,7 +53,7 @@ const RecursiveBlock = ({
   menuStyle,
   menuItem,
 }) => {
-  const { appId, tableSlug } = useParams();
+  const {appId, tableSlug} = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
@@ -85,7 +64,7 @@ const RecursiveBlock = ({
   const pinIsEnabled = useSelector((state) => state.main.pinIsEnabled);
   const auth = store.getState().auth;
   const defaultAdmin = auth.roleInfo.name === "DEFAULT ADMIN";
-  const { i18n } = useTranslation();
+  const {i18n} = useTranslation();
   const defaultLanguage = i18n.language;
   const readPermission = element?.data?.permission?.read;
   const withoutPermission =
@@ -172,7 +151,7 @@ const RecursiveBlock = ({
     }
   };
 
-  const { isLoading } = useMenuListQuery({
+  const {isLoading} = useMenuListQuery({
     params: {
       parent_id: id,
     },
@@ -213,7 +192,7 @@ const RecursiveBlock = ({
     }
   }, []);
 
-  const { mutate: deleteReportSetting } = useMutation(
+  const {mutate: deleteReportSetting} = useMutation(
     (id) => pivotService.deleteReportSetting(id),
     {
       onSuccess: () => {
@@ -223,7 +202,7 @@ const RecursiveBlock = ({
     }
   );
 
-  const { mutate: onDeleteTemplate } = useMutation(
+  const {mutate: onDeleteTemplate} = useMutation(
     (id) =>
       pivotService.deletePivotTemplate({
         id,

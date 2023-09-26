9a60e3b202e0e971c1e07f5ecbdaf2e1a6343df9
Moxrbe <one393752@gmail.com>
2023-09-26 13:19:45 +0500

    chenged microfrontend

--- src/views/Microfrontend/index.jsx
+++ src/views/Microfrontend/index.jsx
@@ -1,17 +1,11 @@
-
-
-
-
-
 import { useParams } from "react-router-dom";
 import MicrofrontendComponent from "../../components/MicrofrontendComponent";
 import { useQuery } from "react-query";
 import microfrontendService from "../../services/microfrontendService";
 import RingLoaderWithWrapper from "../../components/Loaders/RingLoader/RingLoaderWithWrapper";
 
-
 const Microfrontend = () => {
-  const { microfrontendId } = useParams()  
+  const { microfrontendId } = useParams();
 
   const { data, isLoading } = useQuery(
     ["GET_MICROFRONTEND_BY_ID", microfrontendId],
@@ -20,13 +14,14 @@ const Microfrontend = () => {
     }
   );
 
-  const link = data?.url ? `https://${data?.url}/assets/remoteEntry.js` : undefined
+  const link = data?.url
+    ? `https://${data?.url}/assets/remoteEntry.js`
+    : undefined;
 
-  if (isLoading) return <RingLoaderWithWrapper style={{height: "100vh"}}/>
+  if (isLoading) return <RingLoaderWithWrapper style={{ height: "100vh" }} />;
 
-  if (!link) return null
+  if (!link) return null;
 
-  return <MicrofrontendComponent key={link} link={link} />
-
-}
-export default Microfrontend
+  return <MicrofrontendComponent key={link} link={link} />;
+};
+export default Microfrontend;

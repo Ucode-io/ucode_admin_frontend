d49892c68f239e2c47c3ee6d927a6aa0fac5223c
Bahriddin <bahridnurullav@gmail.com>
2023-11-16 15:06:02 +0500

    multiline ui corrections

--- src/components/FormElements/HFTextEditor.jsx
+++ src/components/FormElements/HFTextEditor.jsx
@@ -1,5 +1,5 @@
-import { lazy, Suspense } from "react";
-import { Controller, useWatch } from "react-hook-form";
+import {lazy, Suspense} from "react";
+import {Controller, useWatch} from "react-hook-form";
 
 import RingLoaderWithWrapper from "../Loaders/RingLoader/RingLoaderWithWrapper";
 import "react-quill/dist/quill.snow.css";
@@ -26,42 +26,47 @@ const HFTextEditor = ({
   });
 
   return (
-    <Controller
-      control={control}
-      name={name}
-      defaultValue=""
-      rules={{
-        required: required ? "This is required field" : false,
-        ...rules,
-      }}
-      render={({ field: { onChange, ref }, fieldState: { error } }) => (
-        <Suspense fallback={<RingLoaderWithWrapper />}>
-          <ReactQuill
-            theme="snow"
-            defaultValue={value}
-            onChange={(val) => {
-              onChange(val);
-              isNewTableView && updateObject();
-            }}
-            tabIndex={tabIndex}
-            autoFocus={false}
-            style={{ backgroundColor: `${isTransparent ? "transparent" : ""}` }}
-          />
-        </Suspense>
-        // <TextField
-        //   size="small"
-        //   value={value}
-        //   onChange={(e) =>
-        //     onChange(withTrim ? e.target.value?.trim() : e.target.value)
-        //   }
-        //   name={name}
-        //   error={error}
-        //   fullWidth={fullWidth}
-        //   helperText={!disabledHelperText && error?.message}
-        //   {...props}
-        // />
-      )}
-    ></Controller>
+    <div id="react_quill">
+      <Controller
+        control={control}
+        name={name}
+        defaultValue=""
+        rules={{
+          required: required ? "This is required field" : false,
+          ...rules,
+        }}
+        render={({field: {onChange, ref}, fieldState: {error}}) => (
+          <Suspense fallback={<RingLoaderWithWrapper />}>
+            <ReactQuill
+              theme="snow"
+              defaultValue={value}
+              onChange={(val) => {
+                onChange(val);
+                isNewTableView && updateObject();
+              }}
+              tabIndex={tabIndex}
+              autoFocus={false}
+              style={{
+                backgroundColor: `${isTransparent ? "transparent" : ""}`,
+                width: "300px",
+              }}
+            />
+          </Suspense>
+          // <TextField
+          //   size="small"
+          //   value={value}
+          //   onChange={(e) =>
+          //     onChange(withTrim ? e.target.value?.trim() : e.target.value)
+          //   }
+          //   name={name}
+          //   error={error}
+          //   fullWidth={fullWidth}
+          //   helperText={!disabledHelperText && error?.message}
+          //   {...props}
+          // />
+        )}
+      ></Controller>
+    </div>
   );
 };
 

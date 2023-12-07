74c0af48ca45d6f33e452eaa50e1fb994d04b19b
Bahriddin <bahridnurullav@gmail.com>
2023-12-07 15:14:31 +0500

    map field border

--- src/components/DataTable/TableRow.jsx
+++ src/components/DataTable/TableRow.jsx
@@ -235,7 +235,7 @@ const TableRow = ({
                   </CTableCell>
                 )
             )}
-            <td>
+            <td style={{height: "30px"}}>
               <div
                 style={{
                   display: "flex",

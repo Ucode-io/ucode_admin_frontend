6bf43e34d800e248a10cf5e45ebc6c5e2bcaf6f3
Bahriddin <bahridnurullav@gmail.com>
2023-12-25 15:03:02 +0500

    numberfield zero update

--- src/components/ElementGenerators/CellElementGeneratorForTableView.jsx
+++ src/components/ElementGenerators/CellElementGeneratorForTableView.jsx
@@ -2,8 +2,32 @@ import {Parser} from "hot-formula-parser";
 import {useEffect, useMemo} from "react";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
+import HFAutocomplete from "../FormElements/HFAutocomplete";
+import HFCheckbox from "../FormElements/HFCheckbox";
+import HFColorPicker from "../FormElements/HFColorPicker";
+import HFDatePicker from "../FormElements/HFDatePicker";
+import HFDateTimePicker from "../FormElements/HFDateTimePicker";
+import HFFileUpload from "../FormElements/HFFileUpload";
+import HFFloatField from "../FormElements/HFFloatField";
+import HFFormulaField from "../FormElements/HFFormulaField";
+import HFIconPicker from "../FormElements/HFIconPicker";
+import HFModalMap from "../FormElements/HFModalMap";
+import HFMultipleAutocomplete from "../FormElements/HFMultipleAutocomplete";
+import HFNumberField from "../FormElements/HFNumberField";
+import HFPassword from "../FormElements/HFPassword";
+import HFSwitch from "../FormElements/HFSwitch";
+import HFTextField from "../FormElements/HFTextField";
+import HFTextFieldWithMask from "../FormElements/HFTextFieldWithMask";
+import HFTimePicker from "../FormElements/HFTimePicker";
+import HFVideoUpload from "../FormElements/HFVideoUpload";
+import InventoryBarCode from "../FormElements/InventoryBarcode";
 import CellElementGenerator from "./CellElementGenerator";
-import {fieldTypesComponent} from "./FieldTypesComponents";
+import MultiLineCellFormElement from "./MultiLineCellFormElement";
+import CellManyToManyRelationElement from "./CellManyToManyRelationElement";
+import NewCHFFormulaField from "../FormElements/NewCHFormulaField";
+import CellRelationFormElementForTableView from "./CellRelationFormElementForTable";
+import HFPhotoUpload from "../FormElements/HFPhotoUpload";
+import HFInternationPhone from "../FormElements/HFInternationPhone";
 
 const parser = new Parser();
 
@@ -24,6 +48,7 @@ const CellElementGeneratorForTableView = ({
   isTableView,
   isNewRow,
 }) => {
+  const selectedRow = useSelector((state) => state.selectedRow.selected);
   const userId = useSelector((state) => state.auth.userId);
   const tables = useSelector((state) => state.auth.tables);
   const {i18n} = useTranslation();
@@ -52,7 +77,7 @@ const CellElementGeneratorForTableView = ({
 
       return `${field.slug}`;
     }
-  }, [field, i18n?.language, index, isNewRow]);
+  }, [field, i18n?.language]);
 
   const isDisabled =
     field.attributes?.disabled ||
@@ -72,6 +97,14 @@ const CellElementGeneratorForTableView = ({
         return defaultValue;
       }
     }
+    if (field.relation_type !== "Many2One" || field?.type !== "LOOKUP") {
+      if (Array.isArray(defaultValue)) {
+        return defaultValue[0];
+      } else {
+        return defaultValue;
+      }
+    }
+
     if (field.type === "MULTISELECT" || field.id?.includes("#"))
       return defaultValue;
 
@@ -80,7 +113,7 @@ const CellElementGeneratorForTableView = ({
     const {error, result} = parser.parse(defaultValue);
 
     return error ? undefined : result;
-  }, [field, userId, objectIdFromJWT]);
+  }, [field]);
 
   useEffect(() => {
     tables?.forEach((table) => {
@@ -94,70 +127,508 @@ const CellElementGeneratorForTableView = ({
     if (!row?.[field.slug]) {
       setFormValue(computedSlug, row?.[field.table_slug]?.guid || defaultValue);
     }
-  }, [row, computedSlug, defaultValue, field, setFormValue]);
+  }, [row, computedSlug, defaultValue]);
 
-  const CellElement = fieldTypesComponent[field.type] || CellElementGenerator;
+  switch (field.type) {
+    case "LOOKUP":
+      return (
+        <CellRelationFormElementForTableView
+          relOptions={relOptions}
+          isNewRow={isNewRow}
+          tableView={tableView}
+          disabled={isDisabled}
+          isFormEdit
+          isBlackBg={isBlackBg}
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          field={field}
+          row={row}
+          placeholder={field.attributes?.placeholder}
+          setFormValue={setFormValue}
+          index={index}
+          defaultValue={defaultValue}
+          relationfields={relationfields}
+          data={data}
+        />
+      );
 
-  const maskGenerator = useMemo(() => {
-    if (field.type === "PHONE") {
-      return "(99) 999-99-99";
-    } else if (field.type === "DATE") {
-      return "99.99.9999";
-    } else if (field.type === "DATE_TIME") {
-      return "99.99.9999 99:99";
-    } else if (field.type === "TIME") {
-      return "99:99";
-    } else {
-      return undefined;
-    }
-  }, [field.type]);
+    case "LOOKUPS":
+      return (
+        <CellManyToManyRelationElement
+          relOptions={relOptions}
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          field={field}
+          row={row}
+          placeholder={field.attributes?.placeholder}
+          setFormValue={setFormValue}
+          index={index}
+          defaultValue={defaultValue}
+        />
+      );
 
-  const computedPattern = useMemo(() => {
-    if (field?.type === "EMAIL") {
-      return {
-        pattern: {
-          value: /\S+@\S+\.\S+/,
-          message: "Incorrect email format",
-        },
-      };
-    }
-  }, [field?.type]);
+    case "SINGLE_LINE":
+      return (
+        <HFTextField
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          field={field}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+        />
+      );
+    case "PASSWORD":
+      return (
+        <HFPassword
+          isDisabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          field={field}
+          isTransparent={true}
+          required={field.required}
+          type="password"
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+        />
+      );
 
-  return (
-    <CellElement
-      relOptions={relOptions}
-      isNewRow={isNewRow}
-      tableView={tableView}
-      disabled={isDisabled}
-      isFormEdit
-      isBlackBg={isBlackBg}
-      updateObject={updateObject}
-      isNewTableView={true}
-      control={control}
-      name={computedSlug}
-      field={field}
-      row={row}
-      placeholder={field.attributes?.placeholder}
-      setFormValue={setFormValue}
-      index={index}
-      defaultValue={defaultValue}
-      relationfields={relationfields}
-      data={data}
-      fields={fields}
-      isWrapField={isWrapField}
-      isDisabled={isDisabled}
-      isTransparent={true}
-      type={field.type === "PASSWORD" && "PASSWORD"}
-      mask={maskGenerator}
-      showCopyBtn={false}
-      width={"100%"}
-      options={field?.attributes?.options}
-      computedSlug={computedSlug}
-      fieldsList={fields}
-      isTableView={true}
-      rules={{...computedPattern}}
-    />
-  );
+    case "SCAN_BARCODE":
+      return (
+        <InventoryBarCode
+          control={control}
+          name={computedSlug}
+          fullWidth
+          updateObject={updateObject}
+          isNewTableView={true}
+          setFormValue={setFormValue}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+          field={field}
+          disabled={isDisabled}
+        />
+      );
+    case "PHONE":
+      return (
+        <HFTextFieldWithMask
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          isTransparent={true}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          mask={"(99) 999-99-99"}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "PHOTO":
+      return (
+        <HFPhotoUpload
+          disabled={isDisabled}
+          isFormEdit
+          field={field}
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          isTransparent={true}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "FORMULA":
+      return (
+        <HFFormulaField
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          mask={"(99) 999-99-99"}
+          defaultValue={defaultValue}
+          isTransparent={true}
+        />
+      );
+    case "FORMULA_FRONTEND":
+      return (
+        <NewCHFFormulaField
+          setFormValue={setFormValue}
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          isTableView={true}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          name={computedSlug}
+          fieldsList={fields}
+          disabled={!isDisabled}
+          isTransparent={true}
+          field={field}
+          index={index}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "PICK_LIST":
+      return (
+        <HFAutocomplete
+          disabled={isDisabled}
+          isBlackBg={isBlackBg}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          width="100%"
+          options={field?.attributes?.options}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "INTERNATION_PHONE":
+      return (
+        <HFInternationPhone
+          control={control}
+          isBlackBg={isBlackBg}
+          isTableView={isTableView}
+          name={computedSlug}
+          tabIndex={field?.tabIndex}
+          fullWidth
+          required={field?.required}
+          placeholder={field.attributes?.placeholder}
+          mask={"(99) 999-99-99"}
+          defaultValue={defaultValue}
+          disabled={isDisabled}
+        />
+      );
+
+    case "MULTISELECT":
+      return (
+        <HFMultipleAutocomplete
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          width="100%"
+          required={field.required}
+          field={field}
+          placeholder={field.attributes?.placeholder}
+          isBlackBg={isBlackBg}
+          defaultValue={defaultValue}
+          data={data}
+        />
+      );
+    case "MULTISELECT_V2":
+      return (
+        <HFMultipleAutocomplete
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          width="100%"
+          required={field.required}
+          field={field}
+          placeholder={field.attributes?.placeholder}
+          isBlackBg={isBlackBg}
+          defaultValue={defaultValue}
+          data={data}
+        />
+      );
+
+    case "DATE":
+      return (
+        <HFDatePicker
+          control={control}
+          name={computedSlug}
+          fullWidth
+          updateObject={updateObject}
+          isNewTableView={true}
+          width={"100%"}
+          mask={"99.99.9999"}
+          isFormEdit
+          isBlackBg={isBlackBg}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+          disabled={isDisabled}
+          isTransparent={true}
+        />
+      );
+
+    case "DATE_TIME":
+      return (
+        <HFDateTimePicker
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          showCopyBtn={false}
+          control={control}
+          name={computedSlug}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+          isTransparent={true}
+        />
+      );
+
+    case "TIME":
+      return (
+        <HFTimePicker
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+          isTransparent={true}
+        />
+      );
+
+    case "NUMBER":
+      return (
+        <HFNumberField
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          isBlackBg={isBlackBg}
+          defaultValue={defaultValue}
+          isTransparent={true}
+        />
+      );
+    case "FLOAT":
+      return (
+        <HFFloatField
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          control={control}
+          name={computedSlug}
+          fullWidth
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          isBlackBg={isBlackBg}
+          defaultValue={defaultValue}
+          isTransparent={true}
+        />
+      );
+
+    case "CHECKBOX":
+      return (
+        <HFCheckbox
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          required={field.required}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "SWITCH":
+      return (
+        <HFSwitch
+          disabled={isDisabled}
+          isFormEdit
+          field={field}
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          required={field.required}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "EMAIL":
+      return (
+        <HFTextField
+          disabled={isDisabled}
+          isFormEdit
+          updateObject={updateObject}
+          isNewTableView={true}
+          isBlackBg={isBlackBg}
+          control={control}
+          name={computedSlug}
+          rules={{
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Incorrect email format",
+            },
+          }}
+          fullWidth
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          defaultValue={defaultValue}
+        />
+      );
+
+    case "ICON":
+      return (
+        <HFIconPicker
+          isFormEdit
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          name={computedSlug}
+          required={field.required}
+          defaultValue={defaultValue}
+        />
+      );
+    case "MAP":
+      return (
+        <HFModalMap
+          isTransparent={true}
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          field={field}
+          defaultValue={defaultValue}
+          isFormEdit
+          name={computedSlug}
+          required={field?.required}
+        />
+      );
+
+    case "MULTI_LINE":
+      return (
+        <MultiLineCellFormElement
+          control={control}
+          isWrapField={isWrapField}
+          updateObject={updateObject}
+          isNewTableView={true}
+          computedSlug={computedSlug}
+          field={field}
+          isDisabled={isDisabled}
+        />
+      );
+
+    case "CUSTOM_IMAGE":
+      return (
+        <HFFileUpload
+          isTransparent={true}
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          name={computedSlug}
+          defaultValue={defaultValue}
+          isFormEdit
+          required={field.required}
+        />
+      );
+
+    case "VIDEO":
+      return (
+        <HFVideoUpload
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          name={computedSlug}
+          defaultValue={defaultValue}
+          isFormEdit
+          isBlackBg={isBlackBg}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          isTransparent={true}
+        />
+      );
+
+    case "FILE":
+      return (
+        <HFFileUpload
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          name={computedSlug}
+          defaultValue={defaultValue}
+          isFormEdit
+          isBlackBg={isBlackBg}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          isTransparent={true}
+        />
+      );
+
+    case "COLOR":
+      return (
+        <HFColorPicker
+          control={control}
+          updateObject={updateObject}
+          isNewTableView={true}
+          name={computedSlug}
+          defaultValue={defaultValue}
+          isFormEdit
+          isBlackBg={isBlackBg}
+          required={field.required}
+          placeholder={field.attributes?.placeholder}
+          isTransparent={true}
+        />
+      );
+
+    default:
+      return (
+        <div style={{padding: "0 4px"}}>
+          <CellElementGenerator field={field} row={row} />
+        </div>
+      );
+  }
 };
 
 export default CellElementGeneratorForTableView;

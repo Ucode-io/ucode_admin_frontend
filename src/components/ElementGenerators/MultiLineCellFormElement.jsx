e14b469d5fdd3cca2766dbb4a82b69b02ace5046
Shohrux99 <b.shohrux@mail.ru>
2024-01-04 02:19:43 +0500

    multi line edited

--- src/components/ElementGenerators/MultiLineCellFormElement.jsx
+++ src/components/ElementGenerators/MultiLineCellFormElement.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HFTextEditor from "../FormElements/HFTextEditor";
-import { Box, Button, Modal } from "@mui/material";
+import { Box, Button, Modal, Popover } from "@mui/material";
 import ZoomOutMapIcon from "@mui/icons-material/ZoomOutMap";
 import { useWatch } from "react-hook-form";
 
@@ -18,9 +18,9 @@ const style = {
 };
 
 export default function MultiLineCellFormElement({ control, computedSlug, isWrapField, updateObject, isNewTableView = false, field, isDisabled, ...props }) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const [open, setOpen] = React.useState(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
 
   const value = useWatch({
     control,
@@ -31,6 +31,19 @@ export default function MultiLineCellFormElement({ control, computedSlug, isWrap
     return input.replace(/<[^>]*>/g, "");
   };
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <>
       <Box
@@ -39,6 +52,7 @@ export default function MultiLineCellFormElement({ control, computedSlug, isWrap
         }}
       >
         <p
+          onClick={handleClick}
           style={
             isWrapField
               ? {
@@ -47,18 +61,52 @@ export default function MultiLineCellFormElement({ control, computedSlug, isWrap
                   alignItems: "center",
                   justifyContent: "center",
                   marginRight: "8px",
+                  cursor: "text",
                 }
               : {
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
                   marginRight: "8px",
+                  cursor: "text",
                 }
           }
         >
           {stripHtmlTags(`${value?.slice(0, 200)}${value?.length > 200 ? "..." : ""}` ?? "")}
         </p>
-        <Button
+
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <HFTextEditor
+            control={control}
+            updateObject={updateObject}
+            isNewTableView={isNewTableView}
+            name={computedSlug}
+            tabIndex={field?.tabIndex}
+            fullWidth
+            multiline
+            rows={4}
+            defaultValue={field.defaultValue}
+            disabled={isDisabled}
+            key={computedSlug}
+            isTransparent={true}
+            {...props}
+          />
+        </Popover>
+
+        {/* <Button
           onClick={handleOpen}
           sx={{
             width: "30px",
@@ -67,10 +115,10 @@ export default function MultiLineCellFormElement({ control, computedSlug, isWrap
           }}
         >
           <ZoomOutMapIcon />
-        </Button>
+        </Button> */}
       </Box>
 
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+      {/* <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
         <Box sx={style}>
           <HFTextEditor
             control={control}
@@ -98,7 +146,7 @@ export default function MultiLineCellFormElement({ control, computedSlug, isWrap
             Close
           </Button>
         </Box>
-      </Modal>
+      </Modal> */}
     </>
   );
 }

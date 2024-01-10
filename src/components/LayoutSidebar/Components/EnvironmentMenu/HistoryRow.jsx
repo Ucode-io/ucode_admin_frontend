c515ee00b677d79e430c6244dc740e5cc439dd89
Shohrux99 <b.shohrux@mail.ru>
2024-01-10 14:25:51 +0500

    some changes with label

--- src/components/LayoutSidebar/Components/EnvironmentMenu/HistoryRow.jsx
+++ src/components/LayoutSidebar/Components/EnvironmentMenu/HistoryRow.jsx
@@ -5,9 +5,33 @@ import { useTranslation } from "react-i18next";
 
 export default function HistoryRow({ history, index, handleSelectVersion, selectedVersions }) {
   const { i18n } = useTranslation();
+  const decodedCurrentAttributes = struct.decode(history?.current?.attributes ?? {});
+  const decodedPreviousAttributes = struct.decode(history?.previus?.attributes ?? {});
+  const multiLanguageLabel = `label_${i18n.language}`;
 
   const label = useMemo(() => {
-    return struct.decode(history.current.attributes ?? {})?.[`label_${i18n.language}`];
+    if (history?.action_type === "CREATE" || history?.action_type === "BULKWRITE") {
+      switch (history?.action_source) {
+        case "RELATION":
+          return history?.current?.table_from?.label + " ==> " + history?.current?.table_to?.label;
+        default:
+          return decodedCurrentAttributes[multiLanguageLabel] ?? history.current?.label;
+      }
+    } else if (history?.action_type === "UPDATE") {
+      switch (history?.action_source) {
+        case "RELATION":
+          return history?.current?.table_from?.label + " ==> " + history?.current?.table_to?.label;
+        default:
+          return decodedCurrentAttributes[multiLanguageLabel] ?? decodedCurrentAttributes.label;
+      }
+    } else if (history?.action_type === "DELETE") {
+      switch (history?.action_source) {
+        case "RELATION":
+          return decodedPreviousAttributes[multiLanguageLabel] ?? decodedPreviousAttributes.label;
+        default:
+          return decodedPreviousAttributes[multiLanguageLabel] ?? decodedPreviousAttributes.label;
+      }
+    }
   }, [history, i18n.language]);
 
   return (

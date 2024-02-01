b169f2a3723e11063922e5608c8299f6f25e9514
Bahriddin <bahridnurullav@gmail.com>
2024-02-01 10:41:01 +0500

    table create

--- src/views/Constructor/Tables/Form/index.jsx
+++ src/views/Constructor/Tables/Form/index.jsx
@@ -193,10 +193,11 @@ const ConstructorTablesFormPage = () => {
       })
       .then(() => {
         queryClient.refetchQueries(["MENU"], menuItem?.id);
+        navigate(-1);
       })
       .catch((err) => {
         console.log(err);
-      });
+      })
   };
 
   const createConstructorTable = (data) => {
@@ -210,7 +211,6 @@ const ConstructorTablesFormPage = () => {
     )
       .unwrap()
       .then((res) => {
-        navigate(-1);
         createType(res);
       })
       .catch(() => setBtnLoader(false));

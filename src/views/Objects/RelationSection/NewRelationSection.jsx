e714740cb9728bd4aa8fe81734be9126cafcb5e1
Bahriddin <bahridnurullav@gmail.com>
2024-04-30 15:44:13 +0500

    layout tab label

--- src/views/Objects/RelationSection/NewRelationSection.jsx
+++ src/views/Objects/RelationSection/NewRelationSection.jsx
@@ -347,24 +347,16 @@ const NewRelationSection = ({
                         </>
                       )}
                       <div className="flex align-center gap-2 text-nowrap">
-                        {el?.type === "relation"
-                          ? el?.relation?.attributes?.[
-                              `label_${i18n?.language}`
-                            ] ||
-                            el?.relation?.attributes?.[
-                              `label_to_${i18n?.language}`
-                            ] ||
-                            el?.attributes?.[`label_to_${i18n?.language}`] ||
-                            el?.label
-                          : el?.attributes?.[`label_${i18n.language}`]
-                            ? el?.attributes?.[`label_${i18n.language}`]
-                            : el?.relation?.attributes?.[
-                                  `label_${i18n.language}`
-                                ]
-                              ? el?.relation?.attributes?.[
-                                  `label_${i18n.language}`
-                                ]
-                              : el?.label ?? el?.title}
+                        {(el?.relation && el?.relation?.table_from?.label) ||
+                          el?.relation?.attributes?.[
+                            `label_to_${i18n?.language}`
+                          ] ||
+                          el?.attributes?.[`label_${i18n.language}`] ||
+                          el?.relation?.attributes?.[
+                            `label_${i18n.language}`
+                          ] ||
+                          el?.label ||
+                          el?.title}
                       </div>
                     </Tab>
                   ))}

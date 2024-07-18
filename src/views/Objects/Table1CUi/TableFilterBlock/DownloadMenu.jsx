e61d98df5b1f0a6bd565578cc16ae4025ab3c28d
Bahriddin <bahridnurullav@gmail.com>
2024-07-18 15:39:56 +0500

    excel download is added

--- src/views/Objects/Table1CUi/TableFilterBlock/DownloadMenu.jsx
+++ src/views/Objects/Table1CUi/TableFilterBlock/DownloadMenu.jsx
@@ -1,21 +1,52 @@
 import React, {useState} from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import styles from "./style.module.scss";
-import {Box, Menu, MenuItem, Typography} from "@mui/material";
+import {Box, CircularProgress, Menu, MenuItem, Typography} from "@mui/material";
 import {useNavigate, useParams} from "react-router-dom";
+import constructorObjectService from "../../../../services/constructorObjectService";
+import useFilters from "../../../../hooks/useFilters";
+import {useTranslation} from "react-i18next";
+import useDownloader from "../../../../hooks/useDownloader";
 
-function DownloadMenu({menuItem}) {
-  const {appId} = useParams();
+function DownloadMenu({
+  menuItem,
+  fieldSlugId,
+  fieldSlug,
+  view,
+  visibleFields,
+  sort,
+}) {
+  const {appId, tableSlug} = useParams();
   const [anchorEl, setAnchorEl] = useState(null);
-  const navigate = useNavigate();
+  const {t, i18n} = useTranslation();
   const open = Boolean(anchorEl);
+  const {filters} = useFilters(tableSlug, view?.id);
+  const {download} = useDownloader();
+  const [loader, setLoader] = useState(false);
 
   const handleClick = (e) => setAnchorEl(e.currentTarget);
   const handleClose = () => setAnchorEl(null);
 
-  const navigateToSettingsPage = () => {
-    const url = `/settings/constructor/apps/${appId}/objects/${menuItem?.table_id}/${menuItem?.data?.table?.slug}?menuId=${menuItem?.id}`;
-    navigate(url);
+  const onClick = async () => {
+    try {
+      setLoader(true);
+      const {data} = await constructorObjectService.downloadExcel(tableSlug, {
+        data: {
+          [fieldSlug]: fieldSlugId,
+          ...sort,
+          ...filters,
+          field_ids: visibleFields,
+          language: i18n?.language,
+        },
+      });
+
+      const fileName = `${tableSlug}.xlsx`;
+      // window.open('https://' + data.link, { target: '__blank' })
+      await download({link: "https://" + data.link, fileName});
+    } finally {
+      handleClose();
+      setLoader(false);
+    }
   };
 
   return (
@@ -40,7 +71,9 @@ function DownloadMenu({menuItem}) {
             </Typography>
           </MenuItem>
           <MenuItem
-            onClick={handleClose}
+            onClick={() => {
+              onClick();
+            }}
             // key={index}
             sx={{
               display: "flex",
@@ -51,6 +84,11 @@ function DownloadMenu({menuItem}) {
               sx={{color: "#101828", fontWeight: 500, fontSize: "14px"}}>
               Скачать XLSX
             </Typography>
+            {loader && (
+              <div>
+                <CircularProgress size={24} />
+              </div>
+            )}
           </MenuItem>
           {/* <MenuItem
             onClick={() => {

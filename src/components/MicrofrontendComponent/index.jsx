396db9c9c2cfd0d07ae0b3d9fdaa7c986b1cf0d7
Bahriddin <bahridnurullav@gmail.com>
2024-08-28 14:03:01 +0500

    microfront error

--- src/components/MicrofrontendComponent/index.jsx
+++ src/components/MicrofrontendComponent/index.jsx
@@ -1,5 +1,5 @@
-import { Suspense } from "react";
-import { lazy } from "react";
+import {Suspense} from "react";
+import {lazy} from "react";
 // import empty from "remote_empty_app/empty";
 import RingLoaderWithWrapper from "../Loaders/RingLoader/RingLoaderWithWrapper";
 import SafeComponent from "../SafeComponent";
@@ -8,7 +8,7 @@ const empty = lazy(() => import("remote_empty_app/empty"));
 
 const EMPTY = empty;
 
-const MicrofrontendComponent = ({ link, loginAction }) => {
+const MicrofrontendComponent = ({link, loginAction}) => {
   const RemoteButton = lazy(async () => {
     window.remotesMap[`remote_app_${link}`] = {
       url: link,
@@ -17,7 +17,7 @@ const MicrofrontendComponent = ({ link, loginAction }) => {
     };
 
     const comp = await window.__federation_method_getRemote(
-      `remote_app_${link}`,
+      `remote_app_asdasdasdasdasdsd${link}`,
       "./Page"
     );
     return comp;
@@ -25,9 +25,7 @@ const MicrofrontendComponent = ({ link, loginAction }) => {
 
   return (
     <SafeComponent>
-      <Suspense
-        fallback={<RingLoaderWithWrapper style={{ height: "100vh" }} />}
-      >
+      <Suspense fallback={<RingLoaderWithWrapper style={{height: "100vh"}} />}>
         <RemoteButton loginAction={loginAction} />
       </Suspense>
     </SafeComponent>
--- src/components/SafeComponent/index.jsx
+++ src/components/SafeComponent/index.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import NotInterestedIcon from "@mui/icons-material/NotInterested";
 
 export default class SafeComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = {hasError: false};
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: true };
+    return {hasError: true};
   }
 
   componentDidCatch() {}
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div
+          style={{
+            width: "100%",
+            height: "calc(100vh)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}>
+          <h1>Week internet connection</h1>
+          {/* <div>
+            <NotInterestedIcon style={{color: "red", fontSize: "32px"}} />
+          </div> */}
+        </div>
+      );
     }
 
     return this.props.children;

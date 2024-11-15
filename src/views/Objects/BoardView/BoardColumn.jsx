48e70ee63d2c69d3ef6e31940d193c75dad975ec
Bahriddin <bahridnurullav@gmail.com>
2024-11-15 16:38:47 +0500

    board button

--- src/views/Objects/BoardView/BoardColumn.jsx
+++ src/views/Objects/BoardView/BoardColumn.jsx
@@ -137,6 +137,7 @@ const BoardColumn = ({tab, data = [], fieldsMap, view = []}) => {
 
         <div className={`${styles.columnFooterBlock}`}>
           <Button
+            id={`add_${tab?.value}`}
             variant="contain"
             fullWidth
             onClick={(e) => {

b3f3ea6baef8f1fc8b4a8cb5c7da65fd9d2cc094
Bahriddin <bahridnurullav@gmail.com>
2024-11-22 11:02:12 +0500

    enterprise functions

--- src/views/Objects/AgGridTableView/valueOptionGenerator.js
+++ src/views/Objects/AgGridTableView/valueOptionGenerator.js
@@ -4,6 +4,19 @@ const getColumnEditorParams = (item, columnDef) => {
     columnDef.cellEditorParams = {
       values: item.attributes.options.map((option) => option.label),
     };
+  } else if (item?.type === "NUMBER") {
+    columnDef.valueFormatter = (params) => {
+      return params?.value?.toLocaleString();
+    };
+  } else if (item?.type === "DATE") {
+    columnDef.valueFormatter = (params) => {
+      return new Date(params.value).toLocaleDateString("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      });
+    };
   }
 
   return {};

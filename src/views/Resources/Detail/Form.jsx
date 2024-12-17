f6644f75870ed8a7473da982a1aa83dea21d3640
Bahriddin <bahridnurullav@gmail.com>
2024-12-17 16:16:52 +0500

    env domain

--- src/views/Resources/Detail/Form.jsx
+++ src/views/Resources/Detail/Form.jsx
@@ -61,9 +61,7 @@ const Form = ({
     if (value !== 5) return;
 
     const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
-    // const redirectUri = import.meta.env.VITE_BASE_DOMAIN;
-    const redirectUri =
-      "http://localhost:7777/main/c57eedc3-a954-4262-a0af-376c65b5a280/resources/create";
+    const redirectUri = import.meta.env.VITE_BASE_DOMAIN;
 
     const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo&redirect_uri=${redirectUri}`;
 

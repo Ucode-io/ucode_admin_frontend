5eccbe62c7fe16d5ea37ec4eb9a4662fe638fa03
Bahriddin <bahridnurullav@gmail.com>
2025-01-06 08:47:47 +0500

    error bug fixed

--- src/views/Constructor/OpenFaasFunction/OpenFaasFunctionForm.jsx
+++ src/views/Constructor/OpenFaasFunction/OpenFaasFunctionForm.jsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {useForm} from "react-hook-form";
 import {useDispatch} from "react-redux";
 import {useNavigate, useParams} from "react-router-dom";

67e47d812b59a6d3b1c3cfc28b278546aabdf093
Bahriddin <bahridnurullav@gmail.com>
2025-01-10 15:04:14 +0500

    btn field

--- src/components/FormElements/HFButtonField.jsx
+++ src/components/FormElements/HFButtonField.jsx
@@ -1,9 +1,10 @@
-import {Box, Button} from "@mui/material";
+import {Box, Button, CircularProgress} from "@mui/material";
 import React, {useState} from "react";
 import IconGenerator from "../IconPicker/IconGenerator";
 import request from "../../utils/request";
 import {useNavigate, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
+import {showAlert} from "../../store/alert/alert.thunk";
 
 function HFButtonField({
   row,
@@ -30,52 +31,11 @@ function HFButtonField({
       })
       .then((res) => {
         dispatch(showAlert("Success", "success"));
-
-        let url = res?.link ?? event?.url ?? "";
-        if (res?.status === "error") {
-          dispatch(showAlert(/*res?.data?.message,*/ "error"));
-        } else {
-          if (event?.action_type === "HTTP") {
-            if (event?.attributes?.use_refresh) {
-              navigate("/reloadRelations", {
-                state: {
-                  redirectUrl: window.location.pathname,
-                },
-              });
-            }
-          } else if (url) {
-            Object.entries(res?.data ?? {}).forEach(([key, value]) => {
-              const computedKey = "${" + key + "}";
-              url = url.replaceAll(computedKey, value);
-            });
-          } else if (url.includes("reload:")) {
-            navigate("/reload", {
-              state: {
-                redirectUrl: url,
-              },
-            });
-          } else if (url === "" || url === "reload") {
-            navigate("/reload", {
-              state: {
-                redirectUrl: window.location.pathname,
-              },
-            });
-          } else if (url === "reloadRelations") {
-            navigate("/reloadRelations", {
-              state: {
-                redirectUrl: window.location.pathname,
-              },
-            });
-          } else if (url?.includes("cdn")) {
-            download({link: url, fileName: getLastUnderscorePart(url)});
-          } else {
-            if (url.includes("http") || url.includes("https")) {
-              window.open(url, "_blank");
-            } else {
-              navigate(`/${url}`);
-            }
-          }
-        }
+        navigate("/reloadRelations", {
+          state: {
+            redirectUrl: window.location.pathname,
+          },
+        });
       })
       .finally(() => setBtnLoader(false));
   };
@@ -84,8 +44,15 @@ function HFButtonField({
     <>
       {isTableView ? (
         <Box sx={{width: "100%", height: "100%", textAlign: "center"}}>
-          <Button onClick={invokeFunction} variant="outlined">
-            <IconGenerator icon={field?.attributes?.icon} />
+          <Button
+            disabled={btnLoader}
+            onClick={invokeFunction}
+            variant="outlined">
+            {btnLoader ? (
+              <CircularProgress size={20} />
+            ) : (
+              <IconGenerator icon={field?.attributes?.icon} />
+            )}
           </Button>
         </Box>
       ) : (
@@ -98,8 +65,15 @@ function HFButtonField({
             justifyContent: "center",
           }}>
           <Box sx={{height: "30px", width: "100%"}}></Box>
-          <Button onClick={invokeFunction} variant="outlined">
-            <IconGenerator icon={field?.attributes?.icon} />
+          <Button
+            disabled={btnLoader}
+            onClick={invokeFunction}
+            variant="outlined">
+            {btnLoader ? (
+              <CircularProgress size={20} />
+            ) : (
+              <IconGenerator icon={field?.attributes?.icon} />
+            )}
           </Button>
         </Box>
       )}

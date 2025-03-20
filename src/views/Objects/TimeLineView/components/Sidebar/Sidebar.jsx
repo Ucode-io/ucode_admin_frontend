fd101f87c68682a4c5e231390c12d8a90005c26c
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-03-20 14:28:41 +0500

    add: components for timeline

---
+++ src/views/Objects/TimeLineView/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import cls from "./styles.module.scss";
+import { SidebarButton } from "../SidebarButton";
+import { TimelineRecursiveRow } from "../TimelineRecursiveRow";
+
+export const Sidebar = ({
+  handleCloseSidebar,
+  view,
+  computedData,
+  openedRows,
+  setOpenedRows,
+  fieldsMap,
+  groupByFields,
+  computedColumnsFor,
+  setFocusedDays,
+  datesList,
+  zoomPosition,
+}) => {
+
+  return <div className={cls.sidebar}>
+    <div
+      className={clsx(cls.sidebarHeader)}
+    >
+      <span>Columns</span>
+      <SidebarButton
+        className={cls.sidebarBtn}
+        onClick={handleCloseSidebar}
+      />
+    </div>
+
+    {view?.attributes?.calendar_from_slug !==
+      view?.attributes?.calendar_to_slug && (
+      <div className={cls.sidebar_columns}>
+        {computedData?.map((item, index) => (
+          <TimelineRecursiveRow
+            openedRows={openedRows}
+            setOpenedRows={setOpenedRows}
+            level={0}
+            groupItem={item}
+            fieldsMap={fieldsMap}
+            view={view}
+            groupByFields={groupByFields}
+            computedColumnsFor={computedColumnsFor}
+            setFocusedDays={setFocusedDays}
+            datesList={datesList}
+            zoomPosition={zoomPosition}
+            calendar_from_slug={
+              view?.attributes?.calendar_from_slug
+            }
+            calendar_to_slug={view?.attributes?.calendar_to_slug}
+            visible_field={view?.attributes?.visible_field}
+          />
+        ))}
+      </div>
+    )}
+  </div>
+}
---
+++ src/views/Objects/TimeLineView/components/TimelineRecursiveRow/useTimelineRecursiveRowProps.jsx
@@ -0,0 +1,81 @@
+import {get} from "@ngard/tiny-get";
+import { useEffect, useMemo, useState } from "react";
+import constructorObjectService from "@/services/constructorObjectService";
+
+export const useTimelineRecursiveRowProps = ({
+  item,
+  fieldsMap,
+  openedRows,
+  setOpenedRows,
+  lastLabels = "",
+}) => {
+
+  const [open, setOpen] = useState(false);
+
+  const viewFields = Object.values(fieldsMap)
+    .find((field) => field?.table_slug === item?.group_by_slug)
+    ?.view_fields?.map((field) => field?.slug);
+
+  const handleClick = () => {
+    const isCurrentlyOpen = openedRows.includes(
+      lastLabels?.length ? lastLabels + "." + item?.label : item?.label
+    );
+
+    setOpen(!isCurrentlyOpen);
+
+    if (!isCurrentlyOpen) {
+      setOpenedRows([
+        ...openedRows,
+        lastLabels?.length ? lastLabels + "." + item?.label : item?.label,
+      ]);
+    } else {
+      setOpenedRows(
+        openedRows.filter(
+          (row) =>
+            row !==
+            (lastLabels?.length ? lastLabels + "." + item?.label : item?.label)
+        )
+      );
+    }
+  };
+
+  const computedValue = useMemo(() => {
+    const slugs = viewFields?.map((item) => item) ?? [];
+
+    return slugs
+      .map((slug) =>
+        get(item?.[`${item?.group_by_slug}_id_data`]?.[0], slug, "")
+      )
+      .join(" ");
+  }, [viewFields]);
+
+  useEffect(() => {
+    if (item?.group_by_type === "LOOKUP") {
+      constructorObjectService
+        .getById(item?.group_by_slug, item?.label)
+        .then((res) => {
+          if (res?.data?.response) {
+            // setLabel(res?.data?.response);
+          }
+        });
+    }
+  }, [item]);
+
+  useEffect(() => {
+    if (
+      openedRows.includes(
+        lastLabels?.length ? lastLabels + "." + item?.label : item?.label
+      )
+    ) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [item, openedRows, lastLabels]);
+  
+  return {
+    handleClick,
+    computedValue,
+    open,
+  }
+}

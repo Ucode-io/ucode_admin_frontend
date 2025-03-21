085111497f4a9619ea90d5b5fca7321ae57bf40d
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-03-21 08:44:51 +0500

    change: sidebar ui

--- src/views/Objects/TimeLineView/components/Sidebar/Sidebar.jsx
+++ src/views/Objects/TimeLineView/components/Sidebar/Sidebar.jsx
@@ -17,41 +17,39 @@ export const Sidebar = ({
   zoomPosition,
 }) => {
 
-  return <div className={cls.sidebar}>
-    <div
-      className={clsx(cls.sidebarHeader)}
-    >
-      <span>Columns</span>
-      <SidebarButton
-        className={cls.sidebarBtn}
-        onClick={handleCloseSidebar}
-      />
-    </div>
-
-    {view?.attributes?.calendar_from_slug !==
-      view?.attributes?.calendar_to_slug && (
-      <div className={cls.sidebar_columns}>
-        {computedData?.map((item, index) => (
-          <TimelineRecursiveRow
-            openedRows={openedRows}
-            setOpenedRows={setOpenedRows}
-            level={0}
-            groupItem={item}
-            fieldsMap={fieldsMap}
-            view={view}
-            groupByFields={groupByFields}
-            computedColumnsFor={computedColumnsFor}
-            setFocusedDays={setFocusedDays}
-            datesList={datesList}
-            zoomPosition={zoomPosition}
-            calendar_from_slug={
-              view?.attributes?.calendar_from_slug
-            }
-            calendar_to_slug={view?.attributes?.calendar_to_slug}
-            visible_field={view?.attributes?.visible_field}
-          />
-        ))}
+  return (
+    <div className={cls.group_by}>
+      <div className={clsx(cls.fakeDiv)}>
+        <span>Columns</span>
+        {/* <SidebarButton
+          className={cls.sidebarBtn}
+          onClick={handleCloseSidebar}
+        /> */}
       </div>
-    )}
-  </div>
+
+      {view?.attributes?.calendar_from_slug !==
+        view?.attributes?.calendar_to_slug && (
+        <div className={cls.sidebar_columns}>
+          {computedData?.map((item, index) => (
+            <TimelineRecursiveRow
+              openedRows={openedRows}
+              setOpenedRows={setOpenedRows}
+              level={0}
+              groupItem={item}
+              fieldsMap={fieldsMap}
+              view={view}
+              groupByFields={groupByFields}
+              computedColumnsFor={computedColumnsFor}
+              setFocusedDays={setFocusedDays}
+              datesList={datesList}
+              zoomPosition={zoomPosition}
+              calendar_from_slug={view?.attributes?.calendar_from_slug}
+              calendar_to_slug={view?.attributes?.calendar_to_slug}
+              visible_field={view?.attributes?.visible_field}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }

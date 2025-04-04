cd1bed1e0cab9e29a23a3da011fb52cd1bb3a2cf
Bahriddin <bahridnurullav@gmail.com>
2025-04-04 11:11:04 +0500

    language of billing

--- src/layouts/MainLayout/SubscriptionWarning.jsx
+++ src/layouts/MainLayout/SubscriptionWarning.jsx
@@ -3,9 +3,11 @@ import React, {useMemo} from "react";
 import {useNavigate} from "react-router-dom";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 import {Box, Typography} from "@mui/material";
+import {useTranslation} from "react-i18next";
 
 const SubscriptionWarning = ({projectInfo, handleOpenBilling}) => {
   const navigate = useNavigate();
+  const {t, i18n} = useTranslation();
   const projectStatus = localStorage.getItem("project_status");
   const subscriptionType = projectInfo?.subscription_type;
   const expireDate = projectInfo?.expire_date;
@@ -26,7 +28,7 @@ const SubscriptionWarning = ({projectInfo, handleOpenBilling}) => {
     return (
       <WarningBanner
         onClick={handleOpenBilling}
-        message="Your free trial is ending soon, "
+        message={t("free_trial_ending")}
         daysLeft={daysLeft}
         bgColor="lightblue"
       />
@@ -41,7 +43,7 @@ const SubscriptionWarning = ({projectInfo, handleOpenBilling}) => {
     return (
       <WarningBanner
         onClick={handleOpenBilling}
-        message="Your subscription will expire soon, "
+        message={t("subscribtion_expire_soon")}
         daysLeft={daysLeft}
       />
     );
@@ -55,71 +57,85 @@ const WarningBanner = ({
   message,
   daysLeft,
   bgColor = "rgb(255, 244, 180)",
-}) => (
-  <Box
-    onClick={onClick}
-    sx={{
-      position: "sticky",
-      top: 0,
-      height: "32px",
-      width: "100%",
-      background: bgColor,
-      left: 0,
-      padding: "10px 10px 10px 20px",
-      display: "flex",
-      alignItems: "center",
-      gap: "10px",
-      zIndex: 9,
-      cursor: "pointer",
-      justifyContent: "center",
-    }}>
-    <Box sx={{display: "flex", alignItems: "center"}}>
-      <WarningAmberIcon
-        sx={{color: "#000", fontSize: 20, marginRight: "10px"}}
-      />
-      <Typography sx={{fontSize: "12px", fontWeight: "bold", color: "#000"}}>
-        {message} {daysLeft !== null && <strong>{daysLeft} days left.</strong>}
+}) => {
+  const {t, i18n} = useTranslation();
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        position: "sticky",
+        top: 0,
+        height: "32px",
+        width: "100%",
+        background: bgColor,
+        left: 0,
+        padding: "10px 10px 10px 20px",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        zIndex: 9,
+        cursor: "pointer",
+        justifyContent: "center",
+      }}>
+      <Box sx={{display: "flex", alignItems: "center"}}>
+        <WarningAmberIcon
+          sx={{color: "#000", fontSize: 20, marginRight: "10px"}}
+        />
+        <Typography sx={{fontSize: "12px", fontWeight: "bold", color: "#000"}}>
+          {message}{" "}
+          {daysLeft !== null && (
+            <strong>
+              {daysLeft} {Number(daysLeft) >= 1 ? t("day") : t("days")}{" "}
+              {t("left")}
+            </strong>
+          )}
+        </Typography>
+      </Box>
+      <Typography sx={{fontSize: "12px", color: "#000"}}>
+        <strong style={{textDecoration: "underline"}}>
+          {t("click_here_upgrade")}
+        </strong>
       </Typography>
     </Box>
-    <Typography sx={{fontSize: "12px", color: "#000"}}>
-      Please <strong>Click here</strong> to upgrade your plan.
-    </Typography>
-  </Box>
-);
+  );
+};
 
-const SubscribeExpired = ({onClick = () => {}}) => (
-  <Box
-    onClick={onClick}
-    sx={{
-      position: "sticky",
-      top: 0,
-      height: "32px",
-      width: "100%",
-      background: "#FFBDB8",
-      left: 0,
-      padding: "10px 10px 10px 20px",
-      display: "flex",
-      alignItems: "center",
-      gap: "10px",
-      zIndex: 9,
-      cursor: "pointer",
-      justifyContent: "center",
-    }}>
-    <Box sx={{display: "flex", alignItems: "center"}}>
-      <WarningAmberIcon
-        sx={{color: "#000", fontSize: 20, marginRight: "10px"}}
-      />
-      <Typography sx={{fontSize: "12px", fontWeight: "bold", color: "#000"}}>
-        Your subscription has expired.
+const SubscribeExpired = ({onClick = () => {}}) => {
+  const {t, i18n} = useTranslation();
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        position: "sticky",
+        top: 0,
+        height: "32px",
+        width: "100%",
+        background: "#FFBDB8",
+        left: 0,
+        padding: "10px 10px 10px 20px",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        zIndex: 9,
+        cursor: "pointer",
+        justifyContent: "center",
+      }}>
+      <Box sx={{display: "flex", alignItems: "center"}}>
+        <WarningAmberIcon
+          sx={{color: "#000", fontSize: 20, marginRight: "10px"}}
+        />
+        <Typography sx={{fontSize: "12px", fontWeight: "bold", color: "#000"}}>
+          Your subscription has expired.
+        </Typography>
+      </Box>
+      <Typography sx={{fontSize: "12px", color: "#000"}}>
+        Please renew to continue accessing our services.
+        <strong style={{textDecoration: "underline"}}>
+          Click here to upgrade your plan.
+        </strong>
       </Typography>
     </Box>
-    <Typography sx={{fontSize: "12px", color: "#000"}}>
-      Please renew to continue accessing our services.{" "}
-      <strong style={{textDecoration: "underline"}}>
-        Click here to upgrade your plan.
-      </strong>
-    </Typography>
-  </Box>
-);
+  );
+};
 
 export default SubscriptionWarning;

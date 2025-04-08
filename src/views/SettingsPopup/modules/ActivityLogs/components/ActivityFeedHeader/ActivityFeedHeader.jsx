adce8a56ccad68a704dd1ebdb7079ad15629bc87
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-04-08 09:29:14 +0500

    fix: profile ui

--- src/views/SettingsPopup/modules/ActivityLogs/components/ActivityFeedHeader/ActivityFeedHeader.jsx
+++ src/views/SettingsPopup/modules/ActivityLogs/components/ActivityFeedHeader/ActivityFeedHeader.jsx
@@ -7,6 +7,17 @@ import CRangePickerNew from "@/components/DatePickers/CRangePickerNew";
 import { customStyles } from "@/components/Status";
 import { ContentTitle } from "../../../../components/ContentTitle";
 import { Box } from "@mui/material";
+import { DatePickerInput } from "@mantine/dates";
+import { Portal } from "@mantine/core";
+import InlineSVG from "react-inlinesvg";
+import { CloseIcon } from "@chakra-ui/icons";
+
+export const getColumnIconPath = ({ column }) => {
+  if (column.table_slug === "person") {
+    return "/table-icons/person.svg";
+  }
+  return "/table-icons/date.svg";
+};
 
 export const ActivityFeedHeader = ({
   histories,
@@ -15,16 +26,17 @@ export const ActivityFeedHeader = ({
   activityLan,
   setActionValue,
   isLoading,
+  actionType,
 }) => {
-  const {
-    inputValue,
-    setInputValue,
-    i18n,
-    actionOptions,
-    changeHandler,
-    handleDownloadExcel,
+  const { i18n, handleDownloadExcel } = useActivityFeedHeaderProps({
+    setActionValue,
+    dateFilters,
     actionType,
-  } = useActivityFeedHeaderProps({ setActionValue, dateFilters });
+  });
+
+  const onClearButtonClick = () => {
+    setDateFilters({})(undefined);
+  };
 
   return (
     <>
@@ -40,7 +52,7 @@ export const ActivityFeedHeader = ({
         <Box
           display={"flex"}
           justifyContent={"space-between"}
-          alignItems={"center"}
+          alignItems={"flex-start"}
         >
           <span>
             {generateLangaugeText(
@@ -49,23 +61,74 @@ export const ActivityFeedHeader = ({
               "Activity Logs"
             ) || "Activity Logs"}
           </span>
-          <a
-            onClick={handleDownloadExcel}
-            href="/"
-            style={{
-              fontSize: "14px",
-              cursor: isLoading ? "progress" : "pointer",
-            }}
-          >
-            {generateLangaugeText(
-              activityLan,
-              i18n?.language,
-              "Download Excel"
-            ) || "Download Excel"}
-          </a>
+          <Box display={"flex"} gap={2} alignItems={"center"}>
+            <a
+              onClick={handleDownloadExcel}
+              href="/"
+              style={{
+                fontSize: "14px",
+                cursor: isLoading ? "progress" : "pointer",
+              }}
+            >
+              {generateLangaugeText(
+                activityLan,
+                i18n?.language,
+                "Download Excel"
+              ) || "Download Excel"}
+            </a>
+            <Box position="relative" minWidth="176px">
+              <DatePickerInput
+                styles={{
+                  minWidth: "176px",
+                }}
+                popoverProps={{
+                  withinPortal: false,
+                }}
+                id="dateFieldActivity"
+                type="range"
+                onChange={(value) => {
+                  setDateFilters({
+                    $gte: value[0],
+                    $lt: value[1],
+                  });
+                }}
+                value={[dateFilters.$gte, dateFilters.$lt]}
+                // isClearable={false}
+                clearable
+                placeholder="Date filter"
+                valueFormat="DD.MM.YYYY"
+                rightSection={
+                  dateFilters.$gte && dateFilters.$lt ? (
+                    <CloseIcon
+                      color="#909EAB"
+                      width="9px"
+                      height="9px"
+                      ml="4px"
+                      onClick={(ev) => {
+                        ev.stopPropagation();
+                        onClearButtonClick(ev);
+                      }}
+                      _hover={{ color: "#c00000" }}
+                    />
+                  ) : null
+                }
+                leftSection={
+                  !dateFilters.$gte &&
+                  !dateFilters.$lt && (
+                    <InlineSVG
+                      src={"/table-icons/date.svg"}
+                      width={14}
+                      height={14}
+                      color="#909EAB"
+                    />
+                  )
+                }
+              />
+            </Box>
+          </Box>
         </Box>
       </ContentTitle>
-      <FiltersBlock sideClassName={cls.side} className={cls.filter_block}>
+      {/* <FiltersBlock sideClassName={cls.side} className={cls.filter_block}>
         <Select
           inputValue={inputValue}
           onInputChange={(newInputValue, { action }) => {
@@ -97,13 +160,7 @@ export const ActivityFeedHeader = ({
           }
           blurInputOnSelect
         />
-        <CRangePickerNew
-          onChange={setDateFilters}
-          value={dateFilters}
-          isClearable={false}
-          maxZIndex={true}
-        />
-      </FiltersBlock>
+      </FiltersBlock> */}
     </>
   );
 };

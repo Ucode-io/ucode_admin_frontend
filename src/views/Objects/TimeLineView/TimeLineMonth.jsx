9313efcdf7f563559f514052aaf5d9ec9b5a5a83
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-04-16 14:45:28 +0500

    add: date month

--- src/views/Objects/TimeLineView/TimeLineMonth.jsx
+++ src/views/Objects/TimeLineView/TimeLineMonth.jsx
@@ -1,12 +1,32 @@
 import {Popover, Typography} from "@mui/material";
-import {eachDayOfInterval, format} from "date-fns";
-import React, {useEffect, useState} from "react";
+import {
+  eachDayOfInterval,
+  format,
+  getDay,
+  isValid,
+  isWithinInterval,
+} from "date-fns";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
+import clsx from "clsx";
 
-const TimelineMonthBlock = ({day, zoomPosition, selectedType, focusedDays}) => {
+const TimelineMonthBlock = ({
+  day,
+  zoomPosition,
+  selectedType,
+  focusedDays,
+  month,
+}) => {
   const [anchorEl, setAnchorEl] = useState(null);
+
   const splittedDay = day.split("/");
-  const today = new Date().getDate();
+  const splittedMonth = month?.split(" ")[0];
+  const splittedYear = month?.split(" ")[1];
+
+  const today = format(new Date(), "dd.MM.yyyy");
+
+  const date = `${splittedDay[0]} ${splittedMonth}, ${splittedYear}`;
+
   const handlePopoverOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -22,46 +42,80 @@ const TimelineMonthBlock = ({day, zoomPosition, selectedType, focusedDays}) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
 
-    const dateRange = eachDayOfInterval({start, end});
+    const dateRange = eachDayOfInterval({ start, end });
     return dateRange;
   }
 
-  const [isFocusedDay, setIsFocusedDay] = useState(false);
+  // const [isFocusedDay, setIsFocusedDay] = useState(false);
 
-  useEffect(() => {
-    if (!focusedDays?.length) {
-      setIsFocusedDay(false);
-      return;
-    }
+  // useEffect(() => {
+  //   if (!focusedDays?.length) {
+  //     setIsFocusedDay(false);
+  //     return;
+  //   }
+  //   const dateRange = generateDateRange(focusedDays?.[0], focusedDays?.[1]);
+  //   setIsFocusedDay(
+  //     dateRange
+  //       .map((date) => {
+  //         if (!date) return null;
+  //         return format(date, "dd/EEEE");
+  //       })
+  //       .includes(day)
+  //   );
 
-    const dateRange = generateDateRange(focusedDays?.[0], focusedDays?.[1]);
+  //   setIsFocusedDay();
+  // }, [focusedDays]);
 
-    setIsFocusedDay(
-      dateRange
-        .map((date) => {
-          if (!date) return null;
-          return format(date, "dd/EEEE");
-        })
-        .includes(day)
-    );
-  }, [focusedDays, day]);
+  const targetDate = new Date(date);
+  const [start, end] = focusedDays;
+
+  const isInFocus =
+    isValid(targetDate) &&
+    isValid(start) &&
+    isValid(end) &&
+    isWithinInterval(targetDate, { start, end });
+
+  const isMonday = getDay(new Date(format(new Date(date), "yyyy-MM-dd"))) === 1;
+
+  const isFirstSelectedDay =
+    format(start, "dd.MM.yyyy") === format(new Date(date), "dd.MM.yyyy");
+  const isLastSelectedDay =
+    format(end, "dd.MM.yyyy") === format(new Date(date), "dd.MM.yyyy");
+
+  const isVisible =
+    today === format(new Date(date), "dd.MM.yyyy") ||
+    isMonday ||
+    isLastSelectedDay ||
+    isFirstSelectedDay;
+
   return (
     <>
       <div
         style={{
           minWidth: `${zoomPosition * 20}px`,
-          visibility: selectedType === "month" ? "hidden" : "visible",
-          backgroundColor: isFocusedDay ? "#7f77f1" : "transparent",
-          color: isFocusedDay ? "#fff" : "inherit",
+          color: isInFocus ? "#fff" : "rgba(70, 68, 64, 0.45)",
           position: "relative",
-          borderRight: "1px solid red",
         }}
         aria-owns={open ? "mouse-over-popover" : undefined}
         aria-haspopup="true"
         onMouseEnter={handlePopoverOpen}
         onMouseLeave={handlePopoverClose}
-        className={`${styles.dayBlock} ${(splittedDay[1] === "Saturday" || splittedDay[1] === "Sunday") && selectedType !== "month" ? styles.dayOff : ""}`}>
-        {splittedDay[0]}
+        className={clsx(styles.monthBlockDay, {
+          [styles.monthBlockFocused]: isInFocus,
+          [styles.firstSelectedDay]: isFirstSelectedDay,
+          [styles.lastSelectedDay]: isLastSelectedDay,
+          [styles.todayTopRow]: today === format(new Date(date), "dd.MM.yyyy"),
+        })}
+      >
+        <span
+          style={{
+            visibility: isVisible ? "visible" : "hidden",
+            position: "relative",
+            zIndex: 20,
+          }}
+        >
+          {splittedDay[0]}
+        </span>
       </div>
       <Popover
         id="mouse-over-popover"
@@ -79,8 +133,9 @@ const TimelineMonthBlock = ({day, zoomPosition, selectedType, focusedDays}) => {
           horizontal: "center",
         }}
         onClose={handlePopoverClose}
-        disableRestoreFocus>
-        <Typography sx={{p: 1, background: "#384147", color: "#fff"}}>
+        disableRestoreFocus
+      >
+        <Typography sx={{ p: 1, background: "#384147", color: "#fff" }}>
           {splittedDay[0] + " / " + splittedDay[1]}
         </Typography>
       </Popover>

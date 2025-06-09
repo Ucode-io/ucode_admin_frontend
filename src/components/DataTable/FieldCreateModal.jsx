2fb2a29496e50c778da70890ee2cdb8dd0b95290
Bahriddin <bahridnurullav@gmail.com>
2025-06-09 13:53:06 +0500

    field create error fixed

--- src/components/DataTable/FieldCreateModal.jsx
+++ src/components/DataTable/FieldCreateModal.jsx
@@ -33,6 +33,7 @@ import "./style.scss";
 import {useFieldsListQuery} from "../../services/constructorFieldService";
 import StatusFieldSettings from "../../views/Constructor/Tables/Form/Fields/StatusFieldSettings";
 import {generateLangaugeText} from "../../utils/generateLanguageText";
+import {useState} from "react";
 
 export default function FieldCreateModal({
   tableLan,

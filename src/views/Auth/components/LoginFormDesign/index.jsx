035212eed3a018158d7ed127fb8a8a7fd3f4bcc4
Bahriddin <bahridnurullav@gmail.com>
2025-07-07 16:47:05 +0500

    login correction

--- src/views/Auth/components/LoginFormDesign/index.jsx
+++ src/views/Auth/components/LoginFormDesign/index.jsx
@@ -298,6 +298,7 @@ const LoginFormDesign = ({
       type: googleAuth?.type ? googleAuth?.type : getFormValue?.type,
       sms_id: codeAppValue?.sms_id,
     };
+
     if (
       (Array.isArray(connections) && connections?.length === 0) ||
       connections === undefined
@@ -309,6 +310,8 @@ const LoginFormDesign = ({
         getFormValue?.project_id &&
         getFormValue?.environment_id
       ) {
+        onSubmitDialog(data);
+      } else if (getFormValue?.firebase) {
         onSubmitDialog(data);
       } else if (googleAuth?.type === "google" && googleAuth?.google_token) {
         onSubmitDialog(data);

8f964a98f95d553a2f02883ffd24e752ace58e94
Bahriddin <bahridnurullav@gmail.com>
2025-08-26 08:59:59 +0500

    env_id corrected

--- src/layouts/MainLayout/AddingGroup.jsx
+++ src/layouts/MainLayout/AddingGroup.jsx
@@ -9,14 +9,21 @@ import {
 } from "@mui/material";
 import userService from "../../services/auth/userService";
 import {store} from "../../store";
-import {useLocation, useParams} from "react-router-dom";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {companyActions} from "../../store/company/company.slice";
+import {permissionsActions} from "../../store/permissions/permissions.slice";
+import {authActions} from "../../store/auth/auth.slice";
+import {useDispatch} from "react-redux";
+import authService from "../../services/auth/authService";
 
 function AddingGroup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [loader, setLoader] = useState(false);
   const [open, setOpen] = useState(false);
   const authStore = store.getState();
   const location = useLocation();
-
+  console.log("authStoreauthStore", authStore);
   const query = new URLSearchParams(location?.search);
   const projectId = query.get("project-id");
   const envId = query.get("env_id");
@@ -43,11 +50,41 @@ function AddingGroup() {
 
     userService
       .userInvite(data)
-      .then(() => {
+      .then((res) => {
+        onSelectEnvironment(res);
         console.log("Joined company successfully!");
         setOpen(false);
       })
       .finally(() => setLoader(false));
+  };
+
+  const onSelectEnvironment = (data = {}) => {
+    if (!data?.env_id || !data?.project_id) {
+      console.warn("Missing env_id or project_id:", data);
+      return;
+    }
+
+    const params = {
+      refresh_token: authStore?.auth?.refreshToken,
+      env_id: envId,
+      project_id: data.project_id,
+      for_env: true,
+    };
+    console.log("paramsparams", params);
+
+    dispatch(companyActions.setEnvironmentItem(data));
+    dispatch(companyActions.setEnvironmentId(data.env_id));
+
+    authService
+      .updateToken({...params}, {...params})
+      .then((res) => {
+        dispatch(companyActions.setProjectId(data.project_id));
+        dispatch(permissionsActions.setPermissions(data?.permissions));
+        store.dispatch(authActions.setTokens(data));
+        navigate("/");
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
   };
 
   return (

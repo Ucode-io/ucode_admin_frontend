dfd60f3e119a5499c136118d1ce0c67a066c4a9d
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-10-02 22:09:47 +0500

    fix: icon bug

--- src/components/IconPicker/IconGeneratorIconjs.jsx
+++ src/components/IconPicker/IconGeneratorIconjs.jsx
@@ -2,7 +2,29 @@ import {memo} from "react";
 import SVG from "react-inlinesvg";
 
 const IconGeneratorIconJs = ({icon, size = 20, prefix = "", ...props}) => {
+  let iconName = "";
+
   if (!icon) return null;
+  const splittedByColon = icon.split(":");
+
+  if (splittedByColon.length > 2) {
+    iconName = splittedByColon[1] + ":" + splittedByColon[2];
+  }
+
+  if (iconName) {
+    return (
+      <SVG
+        src={`${import.meta.env.VITE_ICONJS_PICKER_URL}/${iconName}.svg`}
+        width={size}
+        height={size}
+        preProcessor={(code) =>
+          code.replace("path", 'path fill="currentColor"')
+        }
+        {...props}
+      />
+    );
+  }
+
   return Boolean(prefix) ? (
     <SVG
       src={`${import.meta.env.VITE_ICONJS_PICKER_URL}/${prefix}:${icon}.svg`}

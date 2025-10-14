cb9299d5ec6ba82833826724390122a8458fe5c7
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-10-14 12:13:03 +0500

    fix: website link

--- src/components/LayoutSidebar/Components/OldMenuSwitchCase.jsx
+++ src/components/LayoutSidebar/Components/OldMenuSwitchCase.jsx
@@ -3,16 +3,16 @@ import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 export const adminId = `${import.meta.env.VITE_ADMIN_FOLDER_ID}`;
 
 const typeHandlers = {
-  FOLDER: ({appId, navigate}) => {
+  FOLDER: ({ appId, navigate }) => {
     return navigate(`/main/${appId}`);
   },
-  MINIO_FOLDER: ({appId, element, navigate}) =>
+  MINIO_FOLDER: ({ appId, element, navigate }) =>
     navigate(`/main/${appId}/backet/${element?.id}?menuId=${element?.id}`),
-  TABLE: ({appId, element, navigate}) =>
+  TABLE: ({ appId, element, navigate }) =>
     navigate(
       `/main/${element?.parent_id}/object/${element?.data?.table?.slug}?menuId=${element?.id}`
     ),
-  WIKI: ({appId, element, navigate}) =>
+  WIKI: ({ appId, element, navigate }) =>
     navigate(
       `/main/${appId}/docs/note/${element?.parent_id}/${
         element?.attributes?.wiki_id
@@ -20,7 +20,7 @@ const typeHandlers = {
           : element?.wiki_id
       }`
     ),
-  MICROFRONTEND: ({appId, element, navigate}) => {
+  MICROFRONTEND: ({ appId, element, navigate }) => {
     const obj = {};
     element?.attributes?.params.forEach((el) => {
       obj[el.key] = el.value;
@@ -31,13 +31,17 @@ const typeHandlers = {
       search: `?${searchParams.toString()}`,
     });
   },
-  WEBPAGE: ({appId, element, navigate}) =>
+  WEBPAGE: ({ appId, element, navigate }) =>
     navigate(`/main/${appId}/web-page/${element?.data?.webpage?.id}`),
-  USER: ({appId, element, navigate}) =>
+  USER: ({ appId, element, navigate }) =>
     navigate(`/main/${appId}/user-page/${element?.guid}`),
-  PERMISSION: ({appId, element, navigate}) =>
+  PERMISSION: ({ appId, element, navigate }) =>
     navigate(`/main/${appId}/permission/${element?.guid}`),
-  default: ({appId, navigate}) => {
+  LINK: ({ appId, element, navigate }) =>
+    navigate(`/main/${appId}/website`, {
+      state: { url: element?.attributes?.website_link },
+    }),
+  default: ({ appId, navigate }) => {
     return navigate(`/main/${appId}`);
   },
 };

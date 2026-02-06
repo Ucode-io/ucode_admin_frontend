3aa5931eac4c6ae00c2abdf11bd4fa176a05ffad
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2026-02-06 18:17:47 +0500

    add: pagination

--- src/views/SettingsPopup/modules/Models/Models.jsx
+++ src/views/SettingsPopup/modules/Models/Models.jsx
@@ -1,6 +1,6 @@
 import cls from "./styles.module.scss";
-import {useModelsProps} from "./useModelsProps";
-import {Delete} from "@mui/icons-material";
+import { useModelsProps } from "./useModelsProps";
+import { Delete } from "@mui/icons-material";
 import RectangleIconButton from "@/components/Buttons/RectangleIconButton";
 import {
   CTable,
@@ -11,32 +11,49 @@ import {
 } from "@/components/CTable";
 import TableCard from "@/components/TableCard";
 import SearchInput from "@/components/SearchInput";
-import {ContentTitle} from "../../components/ContentTitle";
-import {Box, Button, CircularProgress} from "@mui/material";
+import { ContentTitle } from "../../components/ContentTitle";
+import { Box, Button, CircularProgress } from "@mui/material";
 import clsx from "clsx";
-import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import ChartDb from "../../../ChartDb";
 import ExternalDatabases from "./ExternalDatabases";
-import {useState} from "react";
-import {useQuery} from "react-query";
+import { useState } from "react";
+import { useQuery } from "react-query";
 import conectionDatabaseService from "../../../../services/connectionDatabaseService";
+import { pageToOffset } from "@/utils/pageToOffset";
 // import ExternalDatabases from "../../../ExternalDatabases";
 
 export const Models = () => {
-  const {tables, loader, setSearchText, navigateToEditForm, deleteTable} =
-    useModelsProps();
+  const {
+    tables,
+    loader,
+    setSearchText,
+    navigateToEditForm,
+    deleteTable,
+    page,
+    limit,
+    onPaginationChange,
+    setLimit,
+    pageCount,
+    setPageCount,
+  } = useModelsProps();
   const [selectedConnection, setSelectedConnection] = useState(null);
   const [loadingId, setLoadingId] = useState();
 
-  const {data: connectionTables, refetch} = useQuery(
+  const { data, refetch } = useQuery(
     ["GET_CONNECTION_TABLES"],
     () => {
-      return conectionDatabaseService.getTables(selectedConnection?.id);
+      return conectionDatabaseService.getTables(selectedConnection?.id, {
+        offset: pageToOffset(page, limit),
+        limit,
+      });
     },
     {
       enabled: Boolean(selectedConnection?.id),
-      select: (res) => res?.tables ?? [],
-    }
+      onSuccess: (res) => {
+        setPageCount(Math.ceil(res.count / limit));
+      },
+    },
   );
 
   const trackConnection = (id) => {
@@ -54,9 +71,9 @@ export const Models = () => {
   return (
     <>
       <Tabs>
-        <TabList style={{borderBottom: "none", marginBottom: "10px"}}>
-          <Tab style={{padding: "10px"}}>Models</Tab>
-          <Tab style={{padding: "10px"}}>ChartDB</Tab>
+        <TabList style={{ borderBottom: "none", marginBottom: "10px" }}>
+          <Tab style={{ padding: "10px" }}>Models</Tab>
+          <Tab style={{ padding: "10px" }}>ChartDB</Tab>
           {/* <Tab style={{padding: "10px"}}>External Databases</Tab> */}
         </TabList>
         <TabPanel>
@@ -65,7 +82,8 @@ export const Models = () => {
               <Box
                 display={"flex"}
                 justifyContent="space-between"
-                alignItems={"center"}>
+                alignItems={"center"}
+              >
                 <span>Таблицы</span>
                 <Box display={"flex"} alignItems={"center"} gap="10px">
                   <SearchInput
@@ -81,8 +99,15 @@ export const Models = () => {
               </Box>
             </ContentTitle>
 
-            <TableCard type={"withoutPadding"}>
-              <CTable disablePagination removableHeight={120}>
+            <TableCard>
+              <CTable
+                count={pageCount}
+                dataCount={data?.count ?? tables?.count}
+                currentPage={page}
+                setCurrentPage={onPaginationChange}
+                limit={limit}
+                setLimit={setLimit}
+              >
                 <CTableHead>
                   <CTableCell className={cls.tableHeadCell} width={10}>
                     №
@@ -96,14 +121,15 @@ export const Models = () => {
                 </CTableHead>
                 <CTableBody columnsCount={4} dataLength={1} loader={loader}>
                   {(Boolean(selectedConnection?.id)
-                    ? connectionTables
+                    ? data?.tables
                     : tables?.tables
                   )?.map((element, index) => (
                     <CTableRow key={element.id}>
                       <CTableCell
-                        style={{textAlign: "center"}}
-                        className={cls.tBodyCell}>
-                        {index + 1}
+                        style={{ textAlign: "center" }}
+                        className={cls.tBodyCell}
+                      >
+                        {(page - 1) * limit + index + 1}
                       </CTableCell>
                       <CTableCell className={cls.tBodyCell}>
                         {element.label || element?.table_name}
@@ -127,7 +153,8 @@ export const Models = () => {
                             element?.is_tracked || Boolean(element?.slug)
                               ? "contained"
                               : "outlined"
-                          }>
+                          }
+                        >
                           {loadingId === element.id ? (
                             <CircularProgress size={20} />
                           ) : element?.is_tracked ? (
@@ -139,12 +166,14 @@ export const Models = () => {
                       </CTableCell>
 
                       <CTableCell
-                        className={clsx(cls.tBodyCell, cls.tBodyAction)}>
+                        className={clsx(cls.tBodyCell, cls.tBodyAction)}
+                      >
                         <RectangleIconButton
                           id="delete_btn"
                           color="error"
                           size="small"
-                          onClick={() => deleteTable(element.id)}>
+                          onClick={() => deleteTable(element.id)}
+                        >
                           <Delete color="error" />
                         </RectangleIconButton>
                       </CTableCell>
@@ -156,7 +185,7 @@ export const Models = () => {
           </div>
         </TabPanel>
         <TabPanel>
-          <Box sx={{height: "585px"}}>
+          <Box sx={{ height: "585px" }}>
             <ChartDb />
           </Box>
         </TabPanel>
